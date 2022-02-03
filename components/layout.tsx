6ddb8e9da6b5f17a37d95b26c550c14c7d3b7bcd
Jonny Lu <johnnyluuu@gmail.com>
2022-02-03 22:34:52 +1000

    Newsletter Close: #3

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,10 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import Newsletter from "./Newsletter";
 
 
 const Layout = ({children}) => {
-  return <div><Header /><main>{children}</main><Footer /></div>
+  return <div><Header /><main>{children}</main><Newsletter /><Footer /></div>
 }
 
 
