9ac39279143c61f0c694873c8bdcda8c393129b4
Jonny Lu <johnnyluuu@gmail.com>
2022-03-20 16:20:07 +1000

    Search bannere Close: #7

---
+++ components/Search/index.tsx
@@ -0,0 +1,85 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import styles from "./styles.module.scss";
+
+export type Tab = {
+  name: string;
+  link: string;
+  external?: boolean;
+};
+
+const Search = () => {
+  const router = useRouter();
+  const { type } = router.query;
+  const isPhenotype = type === "phenotype";
+
+  const tabs: Tab[] = [
+    {
+      name: "Genes",
+      link: "/search",
+    },
+    {
+      name: "Phenotypes",
+      link: "/search?type=phenotype",
+    },
+    {
+      name: "Help, news, blog",
+      link: `${process.env.NEXT_PUBLIC_NEWS_SEARCH}/?s=`,
+      external: true,
+    },
+  ];
+  const [tabIndex, setTabIndex] = useState(isPhenotype ? 1 : 0);
+  useEffect(() => {
+    setTabIndex(isPhenotype ? 1 : 0);
+  }, [type]);
+  return (
+    <div className={`${styles.banner} pb-5 -mb-5`}>
+      <Container className={`pb-5 pt-5 ${styles.container}`}>
+        <div className="col-12 col-md-8 ps-4 pe-4">
+          <div className={styles.tabs}>
+            {tabs.map((tab, i) => {
+              const isActive = i === tabIndex;
+              if (tab.external) {
+                return (
+                  <a
+                    className={isActive ? styles.tab__active : styles.tab}
+                    href={tab.link}
+                  >
+                    {tab.name}
+                  </a>
+                );
+              } else {
+                return (
+                  <Link href={tab.link}>
+                    <a
+                      className={isActive ? styles.tab__active : styles.tab}
+                      href="#"
+                    >
+                      {tab.name}
+                    </a>
+                  </Link>
+                );
+              }
+            })}
+          </div>
+          <div className={styles.inputCont}>
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="Search All 7824 Knockout Data..."
+            />
+            <button className={styles.searchBtn}>
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Search;
---
+++ pages/search.tsx
@@ -0,0 +1,7 @@
+import Search from "../components/Search";
+
+const SearchResults = () => {
+  return <Search />;
+};
+
+export default SearchResults;
