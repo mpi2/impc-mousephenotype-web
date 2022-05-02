2a800590048f66f4b9703cf4b94406b072edb87c
Jonny Lu <johnnyluuu@gmail.com>
2022-05-02 10:59:00 +1000

    Image comapator

--- pages/gene/[pid]/images/[parameterName].tsx
+++ pages/gene/[pid]/images/[parameterName].tsx
@@ -1,13 +1,72 @@
-import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowLeftLong,
+  faMagnifyingGlassMinus,
+  faMagnifyingGlassPlus,
+  faRefresh,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import _ from "lodash";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { Alert, Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import Card from "../../../../components/Card";
 import Search from "../../../../components/Search";
 import gData from "../../gene.json";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import styles from "./styles.module.scss";
+
+const ImageViewer = ({ image }) => {
+  return (
+    <TransformWrapper>
+      {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+        <div className={styles.viewer}>
+          <div className={styles.tools}>
+            <button onClick={() => zoomIn()}>
+              <FontAwesomeIcon icon={faMagnifyingGlassPlus} />
+            </button>
+            <button onClick={() => zoomOut()}>
+              <FontAwesomeIcon icon={faMagnifyingGlassMinus} />
+            </button>
+            <button onClick={() => resetTransform()}>
+              <FontAwesomeIcon icon={faRefresh} />
+            </button>
+          </div>
+          <TransformComponent>
+            <img
+              src={image}
+              alt="test"
+              style={{ width: "100%", display: "block" }}
+            />
+          </TransformComponent>
+        </div>
+      )}
+    </TransformWrapper>
+  );
+};
+
+const Column = ({ images }) => {
+  const [selected, setSelected] = useState(0);
+  return (
+    <Row className={`mt-3 ${styles.images}`}>
+      <Col xs={12} style={{ display: "flex" }}>
+        <ImageViewer image={images[selected]?.jpegUrl} />
+      </Col>
+      {images.map((image, i) => (
+        <Col md={4} lg={3} className="mb-3">
+          <div
+            style={{ minHeight: 50, backgroundColor: "#ddd" }}
+            onClick={() => {
+              setSelected(i);
+            }}
+          >
+            <img src={image.thumbnailUrl} style={{ width: "100%" }} />
+          </div>
+        </Col>
+      ))}
+    </Row>
+  );
+};
 
 const ImagesCompare = () => {
   const [geneData, setGeneData] = useState(null);
@@ -40,16 +99,16 @@ const ImagesCompare = () => {
           <h1 style={{ marginBottom: "1rem" }}>
             <strong>{procedureName}</strong> / {parameterName}
           </h1>
-          <p className="grey">{images.length} images</p>
-          <Row className="mt-3">
-            {images.map((image) => (
-              <Col md={4} lg={3} className="mb-3">
-                <div style={{ minHeight: 50, backgroundColor: "#ddd" }}>
-                  <img src={image.thumbnailUrl} style={{ width: "100%" }} />
-                </div>
+          <div>
+            <Row>
+              <Col sm={6}>
+                <Column images={images} />
               </Col>
-            ))}
-          </Row>
+              <Col sm={6}>
+                <Column images={images} />
+              </Col>
+            </Row>
+          </div>
         </Card>
       </Container>
     </>
