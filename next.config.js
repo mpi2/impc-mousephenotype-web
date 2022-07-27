e21a8ba73d1da8f07f795b2ac1ccbfb49edcded4
Jonny Lu <johnnyluuu@gmail.com>
2022-07-27 18:51:27 +1000

    removed redirect rule in next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   //uncomment the following line when deploying with vercel
   // swcMinify: false,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/search",
-        permanent: false,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/search",
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
   experimental: {
     outputStandalone: true,
   },
