77ca7ab4c8ef921f8ae4b069cacd01c08bd349dc
Jonny Lu <johnnyluuu@gmail.com>
2022-08-07 16:13:25 +1000

    Categorical

--- components/Data/Unidimensional.tsx
+++ components/Data/Unidimensional.tsx
@@ -363,6 +363,7 @@ const Unidimensional = () => {
             <h2>Access the results programmatically</h2>
             <p>
               <a
+                target="_blank"
                 className="link"
                 href="https://www.ebi.ac.uk/mi/impc/solr/statistical-result/select?q=*:*&rows=2147483647&sort=p_value+asc&wt=xml&fq=marker_accession_id:%22MGI:1929293%22&fq=phenotyping_center:(%22MRC+Harwell%22)&fq=metadata_group:a8ee4a7178561c567069d111ea7338b8&fq=allele_accession_id:%22MGI:5548707%22&fq=pipeline_stable_id:HRWL_001&fq=parameter_stable_id:IMPC_HEM_037_001&fq=zygosity:homozygote&fq=strain_accession_id:MGI\:2164831"
               >
@@ -372,6 +373,7 @@ const Unidimensional = () => {
             </p>
             <p>
               <a
+                target="_blank"
                 className="link"
                 href="https://www.ebi.ac.uk/mi/impc/solr/genotype-phenotype/select?q=*:*&rows=2147483647&sort=p_value+asc&wt=xml&fq=marker_accession_id:%22MGI:1929293%22&fq=phenotyping_center:(%22MRC+Harwell%22)&fq=allele_accession_id:%22MGI:5548707%22&fq=pipeline_stable_id:HRWL_001&fq=parameter_stable_id:IMPC_HEM_037_001&fq=zygosity:homozygote&fq=strain_accession_id:MGI\:2164831"
               >
@@ -381,6 +383,7 @@ const Unidimensional = () => {
             </p>
             <p>
               <a
+                target="_blank"
                 className="link"
                 href="https://www.mousephenotype.org/data/exportraw?phenotyping_center=MRC%20Harwell&parameter_stable_id=IMPC_HEM_037_001&allele_accession_id=MGI:5548707&strain=MGI:2164831&pipeline_stable_id=HRWL_001&&zygosity=homozygote&"
               >
