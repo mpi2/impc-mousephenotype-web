4621722b56db3db608c0076fe08b22e5ae0c9342
Jonny Lu <johnnyluuu@gmail.com>
2022-08-07 16:28:45 +1000

    Viability

--- components/Data/Viability.tsx
+++ components/Data/Viability.tsx
@@ -1,8 +1,12 @@
-import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowLeftLong,
+  faExternalLinkAlt,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
-import { Col, Row } from "react-bootstrap";
+import { Alert, Col, Row } from "react-bootstrap";
 import Card from "../../components/Card";
+import SortableTable from "../SortableTable";
 
 const Unidimensional = () => {
   const router = useRouter();
@@ -23,6 +27,22 @@ const Unidimensional = () => {
           <h1>
             <strong>Mavs data chart [Viability]</strong>
           </h1>
+          <Alert variant="yellow">
+            <p>Please note:</p>
+            <ul>
+              <li>
+                data for different colonies will be presented separately (e.g.
+                different alleles; same allele but different background strain;
+                same allele but in different phenotyping centers)
+              </li>
+              <li>
+                phenotype calls are made when a statistically significant
+                abnormal phenotype is detected (that is, preweaning lethality or
+                absence of expected number of homozygote pups based on Mendelian
+                ratios)
+              </li>
+            </ul>
+          </Alert>
         </div>
         <h2>Description of the experiments performed</h2>
         <Row>
@@ -87,42 +107,93 @@ const Unidimensional = () => {
         </Row>
       </Card>
       <Row>
-        <Col lg={5}>
+        <Col lg={4}>
           <Card>
-            <h2 className="primary">[Insert box plot]</h2>
+            <h2 className="primary">[Insert total counts pie chart]</h2>
           </Card>
         </Col>
-        <Col lg={7}>
+        <Col lg={4}>
           <Card>
-            <h2 className="primary">[Insert scatter plot]</h2>
+            <h2 className="primary">[Insert male counts pie chart]</h2>
           </Card>
         </Col>
-        <Col lg={6}>
+        <Col lg={4}>
           <Card>
-            <h2>Results of statistical analysis</h2>
-          </Card>
-        </Col>
-        <Col lg={6}>
-          <Card>
-            <h2>Summary statistics of all data in the dataset</h2>
+            <h2 className="primary">[Insert female counts pie chart]</h2>
           </Card>
         </Col>
         <Col lg={6}>
           <Card>
-            <h2>Statistical method</h2>
+            <SortableTable
+              headers={[
+                { width: 6, label: "", disabled: true },
+                { width: 1, label: "WT", disabled: true },
+                { width: 1, label: "Het", disabled: true },
+                { width: 1, label: "Hom", disabled: true },
+                { width: 1, label: "Hemi", disabled: true },
+                { width: 1, label: "Total", disabled: true },
+              ]}
+            >
+              <tr>
+                <td>Male and female</td>
+                <td>39</td>
+                <td>104</td>
+                <td>31</td>
+                <td></td>
+                <td>174</td>
+              </tr>
+              <tr>
+                <td>Male</td>
+                <td>17</td>
+                <td>50</td>
+                <td>17</td>
+                <td></td>
+                <td>84</td>
+              </tr>
+              <tr>
+                <td>Female</td>
+                <td>22</td>
+                <td>54</td>
+                <td>14</td>
+                <td>N/A</td>
+                <td>90</td>
+              </tr>
+            </SortableTable>
           </Card>
         </Col>
         <Col lg={6}>
-          <Card>
-            <h2>Windowing parameters</h2>
-          </Card>
           <Card>
             <h2>Access the results programmatically</h2>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <h2>Download all the data</h2>
+            <p>
+              <a
+                target="_blank"
+                className="link"
+                href="https://www.ebi.ac.uk/mi/impc/solr/statistical-result/select?q=*:*&rows=2147483647&sort=p_value+asc&wt=xml&fq=marker_accession_id:%22MGI:1929293%22&fq=phenotyping_center:(%22MRC+Harwell%22)&fq=metadata_group:a8ee4a7178561c567069d111ea7338b8&fq=allele_accession_id:%22MGI:5548707%22&fq=pipeline_stable_id:HRWL_001&fq=parameter_stable_id:IMPC_HEM_037_001&fq=zygosity:homozygote&fq=strain_accession_id:MGI\:2164831"
+              >
+                Statistical result raw XML{" "}
+                <FontAwesomeIcon icon={faExternalLinkAlt} />
+              </a>
+            </p>
+            <p>
+              <a
+                target="_blank"
+                className="link"
+                href="https://www.ebi.ac.uk/mi/impc/solr/genotype-phenotype/select?q=*:*&rows=2147483647&sort=p_value+asc&wt=xml&fq=marker_accession_id:%22MGI:1929293%22&fq=phenotyping_center:(%22MRC+Harwell%22)&fq=allele_accession_id:%22MGI:5548707%22&fq=pipeline_stable_id:HRWL_001&fq=parameter_stable_id:IMPC_HEM_037_001&fq=zygosity:homozygote&fq=strain_accession_id:MGI\:2164831"
+              >
+                Genotype phenotype raw XML{" "}
+                <FontAwesomeIcon icon={faExternalLinkAlt} />
+              </a>
+            </p>
+            <p>
+              <a
+                target="_blank"
+                className="link"
+                href="https://www.mousephenotype.org/data/exportraw?phenotyping_center=MRC%20Harwell&parameter_stable_id=IMPC_HEM_037_001&allele_accession_id=MGI:5548707&strain=MGI:2164831&pipeline_stable_id=HRWL_001&&zygosity=homozygote&"
+              >
+                PhenStat-ready raw experiment data{" "}
+                <FontAwesomeIcon icon={faExternalLinkAlt} />
+              </a>
+            </p>
           </Card>
         </Col>
       </Row>
