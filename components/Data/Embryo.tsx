fb31da0b08debe7112338d147d7db0222987626a
Jonny Lu <johnnyluuu@gmail.com>
2022-08-07 16:54:57 +1000

    Embryo

--- components/Data/Embryo.tsx
+++ components/Data/Embryo.tsx
@@ -1,8 +1,14 @@
-import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowLeftLong,
+  faDownload,
+  faExternalLinkAlt,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
-import { Col, Row } from "react-bootstrap";
+import { Alert, Button, Col, Row } from "react-bootstrap";
 import Card from "../../components/Card";
+import SortableTable from "../SortableTable";
 
 const Unidimensional = () => {
   const router = useRouter();
@@ -21,7 +27,7 @@ const Unidimensional = () => {
             </a>
           </button>
           <h1>
-            <strong>Mavs data charts [Embryo]</strong>
+            <strong>Gross Morphology Embryo E14.5-E15.5 / Embryo Size</strong>
           </h1>
         </div>
         <h2>Description of the experiments performed</h2>
@@ -87,42 +93,133 @@ const Unidimensional = () => {
         </Row>
       </Card>
       <Row>
-        <Col lg={5}>
+        <Col lg={12}>
           <Card>
-            <h2 className="primary">[Insert box plot]</h2>
-          </Card>
-        </Col>
-        <Col lg={7}>
-          <Card>
-            <h2 className="primary">[Insert scatter plot]</h2>
+            <h2 className="primary">[Insert chart]</h2>
           </Card>
         </Col>
         <Col lg={6}>
           <Card>
             <h2>Results of statistical analysis</h2>
+            <Alert variant="green">
+              <p className="mb-0">
+                <strong>Combined Male and Female P value</strong>
+              </p>
+              <p>5.76×10-36</p>
+              <p className="mb-0">
+                <strong>Males only</strong>
+              </p>
+              <p>7.57×10-21</p>
+              <p className="mb-0">
+                <strong>Females only</strong>
+              </p>
+              <p>4.60×10-16</p>
+              <p className="mb-0">
+                <strong>Classification</strong>
+              </p>
+              <p>
+                With phenotype threshold value 1e-04 - significant in males,
+                females and in combined dataset
+              </p>
+            </Alert>
           </Card>
         </Col>
         <Col lg={6}>
           <Card>
-            <h2>Summary statistics of all data in the dataset</h2>
+            <h2>Counts by sample type</h2>
+            <SortableTable
+              headers={[
+                { width: 5, label: "Sample type", disabled: true },
+                { width: 2, label: "Abnormal", disabled: true },
+                { width: 2, label: "Normal", disabled: true },
+                { width: 3, label: "Unobservable", disabled: true },
+              ]}
+            >
+              <tr>
+                <td>Female Control</td>
+                <td>2733 </td>
+                <td>1</td>
+                <td>20</td>
+              </tr>
+              <tr>
+                <td>Female homozygote</td>
+                <td>1</td>
+                <td>0</td>
+                <td>8</td>
+              </tr>
+              <tr>
+                <td>Male Control</td>
+                <td>2756</td>
+                <td>1</td>
+                <td>21</td>
+              </tr>
+              <tr>
+                <td>Male homozygote </td>
+                <td>2</td>
+                <td>0</td>
+                <td>11</td>
+              </tr>
+            </SortableTable>
           </Card>
         </Col>
         <Col lg={6}>
           <Card>
             <h2>Statistical method</h2>
+            <p>Supplied as data</p>
           </Card>
         </Col>
         <Col lg={6}>
           <Card>
-            <h2>Windowing parameters</h2>
-          </Card>
-          <Card>
             <h2>Access the results programmatically</h2>
+            <p>
+              <a
+                className="link"
+                target="_blank"
+                href="https://www.ebi.ac.uk/mi/impc/solr/statistical-result/select?q=*:*&rows=2147483647&sort=p_value+asc&wt=xml&fq=marker_accession_id:%22MGI:1929293%22&fq=phenotyping_center:(%22MRC+Harwell%22)&fq=metadata_group:a8ee4a7178561c567069d111ea7338b8&fq=allele_accession_id:%22MGI:5548707%22&fq=pipeline_stable_id:HRWL_001&fq=parameter_stable_id:IMPC_HEM_037_001&fq=zygosity:homozygote&fq=strain_accession_id:MGI\:2164831"
+              >
+                Statistical result raw XML{" "}
+                <FontAwesomeIcon icon={faExternalLinkAlt} />
+              </a>
+            </p>
+            <p>
+              <a
+                className="link"
+                target="_blank"
+                href="https://www.ebi.ac.uk/mi/impc/solr/genotype-phenotype/select?q=*:*&rows=2147483647&sort=p_value+asc&wt=xml&fq=marker_accession_id:%22MGI:1929293%22&fq=phenotyping_center:(%22MRC+Harwell%22)&fq=allele_accession_id:%22MGI:5548707%22&fq=pipeline_stable_id:HRWL_001&fq=parameter_stable_id:IMPC_HEM_037_001&fq=zygosity:homozygote&fq=strain_accession_id:MGI\:2164831"
+              >
+                Genotype phenotype raw XML{" "}
+                <FontAwesomeIcon icon={faExternalLinkAlt} />
+              </a>
+            </p>
+            <p>
+              <a
+                className="link"
+                target="_blank"
+                href="https://www.mousephenotype.org/data/exportraw?phenotyping_center=MRC%20Harwell&parameter_stable_id=IMPC_HEM_037_001&allele_accession_id=MGI:5548707&strain=MGI:2164831&pipeline_stable_id=HRWL_001&&zygosity=homozygote&"
+              >
+                PhenStat-ready raw experiment data{" "}
+                <FontAwesomeIcon icon={faExternalLinkAlt} />
+              </a>
+            </p>
           </Card>
         </Col>
         <Col>
           <Card>
             <h2>Download all the data</h2>
+            <p>
+              Export data as:{" "}
+              <Button>
+                <FontAwesomeIcon icon={faDownload} /> TSV
+              </Button>{" "}
+              or{" "}
+              <Button>
+                <FontAwesomeIcon icon={faDownload} /> XLS
+              </Button>{" "}
+            </p>
+            <p className="grey">
+              <FontAwesomeIcon icon={faInfoCircle} /> NOTE: Data from all charts
+              will be aggregated into one download file.
+            </p>
           </Card>
         </Col>
       </Row>
