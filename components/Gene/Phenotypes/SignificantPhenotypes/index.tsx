6936e2ba13ae7be5a378b6869aa5a73ea63e5b69
Jonny Lu <johnnyluuu@gmail.com>
2023-01-30 23:24:42 +1000

    Fixed significant phenotypes table

--- components/Gene/Phenotypes/SignificantPhenotypes/index.tsx
+++ components/Gene/Phenotypes/SignificantPhenotypes/index.tsx
@@ -2,7 +2,6 @@ import { faVenus, faMars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { Table } from "react-bootstrap";
 import { BodySystem } from "../../../BodySystemIcon";
 import Pagination from "../../../Pagination";
 import SortableTable from "../../../SortableTable";
@@ -15,12 +14,14 @@ const SignificantPhenotypes = ({ data }) => {
     data?.map((d) => ({
       ...d,
       pValue: Number(d.pValue),
-      topLevelPhenotype: d.topLevelPhenotype?.map((t) => t.name),
+      topLevelPhenotype: d.topLevelPhenotypes[0]?.name,
+      phenotype: d.phenotype.name,
+      id: d.phenotype.id,
     })) || [];
   const [sorted, setSorted] = useState<any[]>(null);
 
   useEffect(() => {
-    setSorted(_.orderBy(processed, "parameterName", "asc"));
+    setSorted(_.orderBy(processed, "phenotype", "asc"));
   }, [data]);
 
   if (!data) {
@@ -34,16 +35,16 @@ const SignificantPhenotypes = ({ data }) => {
           doSort={(sort) => {
             setSorted(_.orderBy(processed, sort[0], sort[1]));
           }}
-          defaultSort={["parameterName", "asc"]}
+          defaultSort={["phenotype", "asc"]}
           headers={[
-            { width: 3, label: "Phenotype", field: "parameterName" },
+            { width: 3, label: "Phenotype", field: "phenotype" },
             {
-              width: 1,
+              width: 2,
               label: "System",
               field: "topLevelPhenotype",
             },
             { width: 2, label: "Allele", field: "alleleSymbol" },
-            { width: 2, label: "Zyg", field: "zygosity" },
+            { width: 1, label: "Zyg", field: "zygosity" },
             { width: 1, label: "Sex", field: "sex" },
             { width: 1, label: "Life Stage", field: "lifeStageName" },
             { width: 2, label: "P Value", field: "pValue" },
@@ -55,15 +56,15 @@ const SignificantPhenotypes = ({ data }) => {
               <tr>
                 <td>
                   <Link href="/data/charts?accession=MGI:2444773&allele_accession_id=MGI:6276904&zygosity=homozygote&parameter_stable_id=IMPC_DXA_004_001&pipeline_stable_id=UCD_001&procedure_stable_id=IMPC_DXA_001&parameter_stable_id=IMPC_DXA_004_001&phenotyping_center=UC%20Davis">
-                    <strong className={styles.link}>{d.parameterName}</strong>
+                    <strong className={styles.link}>
+                      {_.capitalize(d.phenotype)}
+                    </strong>
                   </Link>
                 </td>
                 <td>
-                  <BodySystem
-                    name={d.topLevelPhenotype?.[0]}
-                    color="primary"
-                    noSpacing
-                  />
+                  {d.topLevelPhenotypes?.map(({ name }) => (
+                    <BodySystem name={name} color="primary" noSpacing />
+                  ))}
                 </td>
                 <td>
                   {allele[0]}
@@ -71,12 +72,16 @@ const SignificantPhenotypes = ({ data }) => {
                 </td>
                 <td>{d.zygosity}</td>
                 <td>
-                  <FontAwesomeIcon
-                    icon={d.sex == "female" ? faVenus : faMars}
-                  />{" "}
+                  {(d.sex === "male" || d.sex === "female") && (
+                    <>
+                      <FontAwesomeIcon
+                        icon={d.sex == "female" ? faVenus : faMars}
+                      />{" "}
+                    </>
+                  )}
                   {d.sex}
                 </td>
-                <td>{d.lifeStageName[0]}</td>
+                <td>{d.lifeStageName}</td>
                 <td>
                   {!!d.pValue
                     ? Math.round(-Math.log10(Number(d.pValue)) * 1000) / 1000
