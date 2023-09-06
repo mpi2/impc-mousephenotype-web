3c45d163544531fda88f8be13486489c96436d6e
Francisco Peña <francisco@ebi.ac.uk>
2023-09-06 16:13:47 +0100

    remove return type from api service function

--- api-service/index.ts
+++ api-service/index.ts
@@ -2,7 +2,7 @@
 const API_URL = process.env.NEXT_PUBLIC_API_ROOT || "";
 const MOCKING_ENABLED = process.env.NEXT_PUBLIC_API_MOCKING === 'enabled';
 
-export async function fetchAPI(query: string): Promise {
+export async function fetchAPI(query: string) {
   const endpointURL = MOCKING_ENABLED ? query : API_URL + query;
   const response = await fetch(endpointURL);
   if (!response.ok) {
