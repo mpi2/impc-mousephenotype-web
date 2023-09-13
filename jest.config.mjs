36522532f6ec4c6138b19013a997d2cf2620e0e4
Francisco Peña <francisco@ebi.ac.uk>
2023-09-13 16:38:22 +0100

    added jest packages and default config

---
+++ jest.config.mjs
@@ -0,0 +1,11 @@
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+    dir: './',
+});
+
+/** @type {import('jest').Config} */
+const config = {
+    testEnvironment: 'jest-environment-jsdom',
+}
+export default createJestConfig(config)
