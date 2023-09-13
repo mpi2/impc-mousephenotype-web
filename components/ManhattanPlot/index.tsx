a5c56dc73fc0867b6636358bc28cb8dfd9b9c94f
Francisco Peña <francisco@ebi.ac.uk>
2023-09-13 16:14:09 +0100

    added tooltips to Manhattan plot
    based on Locuszoom

--- components/ManhattanPlot/index.tsx
+++ components/ManhattanPlot/index.tsx
@@ -11,17 +11,88 @@ import { chartColors } from "../../utils/chart";
 import annotationPlugin from "chartjs-plugin-annotation";
 import { useQuery } from "@tanstack/react-query";
 import { fetchAPI } from "../../api-service";
-import mockData from "../../mocks/data/phenotypes/MP:0012361/gwas.json";
 import { useRouter } from "next/router";
-import { element } from "prop-types";
+import { useRef, useState } from "react";
+import { isEqual } from 'lodash';
+import mockData from "../../mocks/data/phenotypes/MP:0012361/gwas.json";
+import styles from './styles.module.scss';
+import { formatPValue } from "../../utils";
 
 ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend, annotationPlugin);
+
+type TooltipProps = {
+  tooltip: {
+    opacity: number;
+    top: number;
+    left: number;
+    chromosome: string,
+    genes: Array<{ mgiGeneAccessionId: string, geneSymbol: string, pValue: number }>,
+  };
+  offsetX: number;
+  offsetY: number;
+  onClick: () => void;
+};
+
+const DataTooltip = ({tooltip, offsetY, offsetX, onClick}: TooltipProps) => {
+  const getChromosome = () => {
+    if (tooltip.chromosome === '20') {
+      return 'X';
+    } else if (tooltip.chromosome === '21') {
+      return 'Y';
+    }
+    return tooltip.chromosome;
+  }
+  return (
+    <div
+      className={`${styles.tooltip} ${tooltip.opacity === 0 ? styles.noVisible: styles.visible }`}
+      style={{ top: tooltip.top + offsetX, left: tooltip.left + offsetY, opacity: tooltip.opacity }}
+    >
+      <button className={styles.closeBtn} onClick={onClick}>×</button>
+      <span><strong>Chr: </strong>{ getChromosome() }</span>
+      <ul>
+        { tooltip.genes.map(gene => (
+          <li key={gene.mgiGeneAccessionId}>
+            Gene:&nbsp;
+            <a className="primary link" target="_blank" href={`/genes/${gene.mgiGeneAccessionId}`}>
+              {gene.geneSymbol}
+            </a>
+            <br/>
+            P-value: {formatPValue(gene.pValue)}
+          </li>
+        )) }
+      </ul>
+    </div>
+  )
+}
 
 const transformPValue = (value: number) => -Math.log10(value);
 const ManhattanPlot = ({ phenotypeId }) => {
   const router = useRouter();
+  const chartRef = useRef(null);
+  const [hoverTooltip, setHoverTooltip] = useState({
+    opacity: 0,
+    top: 0,
+    left: 0,
+    chromosome: '',
+    genes: [],
+  });
+  const [clickTooltip, setClickTooltip] = useState({
+    opacity: 0,
+    top: 0,
+    left: 0,
+    chromosome: '',
+    genes: [],
+  });
   const ticks = [];
   let originalTicks = [];
+
+  const calculateTooltipXPos = (pos: number) => {
+    const canvasWidth = chartRef.current.width;
+    if (pos >= canvasWidth / 2) {
+      return pos - (175 * 1.03);
+    }
+    return pos;
+  }
 
   const options = {
     scales: {
@@ -39,7 +110,13 @@ const ManhattanPlot = ({ phenotypeId }) => {
         afterTickToLabelConversion: axis => {
           if (ticks.length) {
             axis.ticks.forEach(tick => {
-              tick.label = originalTicks.find(t => t.value === tick.value).label;
+              let label = originalTicks.find(t => t.value === tick.value).label;
+              if (label === '20') {
+                label = 'X';
+              } else if (label === '21') {
+                label = 'Y';
+              }
+              tick.label = label;
             });
           }
         }
@@ -62,12 +139,39 @@ const ManhattanPlot = ({ phenotypeId }) => {
         }
       },
       tooltip: {
-        displayColors: false,
-        callbacks: {
-          title: items => 'Chromosome: ' + items[0].raw.chromosome,
-          label: item => 'Gene symbol: ' + item.raw.geneSymbol,
-          footer: items => 'P-value: ' + items[0].raw.pValue.toExponential(3)
-        }
+        enabled: false,
+        mode: 'point',
+        external: context => {
+          const tooltipModel = context.tooltip;
+          if (!chartRef || !chartRef.current) return;
+          if (tooltipModel.opacity === 0) {
+            if (hoverTooltip.opacity !== 0) {
+              setHoverTooltip(prev => ({ ...prev, opacity: 0 }));
+            }
+            return;
+          }
+          const newTooltipData = {
+            opacity: 1,
+            left: calculateTooltipXPos(tooltipModel.caretX),
+            top: tooltipModel.caretY,
+            chromosome: tooltipModel.dataPoints[0].dataset.label,
+            genes: tooltipModel.dataPoints.map(({ raw }) => ({
+              geneSymbol: raw.geneSymbol,
+              mgiGeneAccessionId: raw.mgiGeneAccessionId,
+              pValue: raw.pValue
+            })),
+          };
+          if (isEqual(newTooltipData.genes, clickTooltip.genes)) {
+            // if tooltip opened by click has the same data as tooltip opened by hover
+            // that would mean the user just clicked on a point, so we should
+            // render only one tooltip
+            setHoverTooltip(prev => ({ ...prev, opacity: 0 }));
+            return;
+          }
+          if (!isEqual(hoverTooltip, newTooltipData)) {
+            setHoverTooltip(newTooltipData);
+          }
+        },
       }
     },
     elements: {
@@ -75,14 +179,29 @@ const ManhattanPlot = ({ phenotypeId }) => {
         pointBackgroundColor: ctx => ctx.raw.y >= 4 ? '#FFA500' : '#00FFFF'
       }
     },
-    onHover:
-      (e, elements) => !!elements.length ? e.native.target.style.cursor = 'pointer' : e.native.target.style.cursor = 'auto',
+    onHover: (e, elements) => !!elements.length ? e.native.target.style.cursor = 'pointer' : e.native.target.style.cursor = 'auto',
     onClick: (e, elements) =>  {
       if (elements.length) {
-        const gene = elements[0].element?.$context?.raw;
-        router.push(`/genes/${gene.mgiGeneAccessionId}`);
+        const newTooltipData = {
+          opacity: 1,
+          // we assume if onClick is triggered, the mouse is already hovering a point and
+          // the hovering tooltip have appeared, we use the coordinates to minimize the change
+          left: hoverTooltip.left,
+          top: hoverTooltip.top,
+          chromosome: elements[0].element['$context'].raw.chromosome,
+          genes: elements.map(point => point.element['$context'].raw).map(rawData => {
+            return {
+              geneSymbol: rawData.geneSymbol,
+              mgiGeneAccessionId: rawData.mgiGeneAccessionId,
+              pValue: rawData.pValue
+            };
+          })
+        };
+        if (!isEqual(clickTooltip, newTooltipData)) {
+          setClickTooltip(newTooltipData);
+        }
       }
-    }
+    },
   };
 
   const { data } = useQuery({
@@ -140,7 +259,19 @@ const ManhattanPlot = ({ phenotypeId }) => {
   }
 
   return (
-    <Scatter options={options as any} data={data} />
+    <div className={styles.chartWrapper}>
+      <Scatter ref={chartRef} options={options as any} data={data} />
+      <DataTooltip
+        tooltip={clickTooltip}
+        offsetX={0}
+        offsetY={10}
+        onClick={() =>
+          // reset genes and chromosome data to show hovering tooltip
+          setClickTooltip(prevState => ({ ...prevState, genes:[], chromosome: '', opacity: 0 })
+        )}
+      />
+      <DataTooltip tooltip={hoverTooltip} offsetX={0} offsetY={10} onClick={() => {}} />
+    </div>
   )
 };
 
