27f2e1e40703d15d5282e14eaf46ca5e2e624366
Francisco Peña <francisco@ebi.ac.uk>
2023-11-13 09:58:46 +0000

    WIP - integrate new endpoints to display images, add filtering

--- pages/genes/[pid]/images/[parameterStableId].tsx
+++ pages/genes/[pid]/images/[parameterStableId].tsx
@@ -3,20 +3,22 @@ import {
   faMagnifyingGlassMinus,
   faMagnifyingGlassPlus,
   faRefresh,
+  faVenus,
+  faMars,
+  faMarsAndVenus
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import _ from "lodash";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { Alert, Col, Container, Row } from "react-bootstrap";
+import { useState } from "react";
+import { Badge, Col, Container, Row } from "react-bootstrap";
 import Card from "@/components/Card";
 import Search from "@/components/Search";
-import gData from "../../gene.json";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import styles from "./styles.module.scss";
 import { useQuery } from "@tanstack/react-query";
 import { fetchAPI } from "@/api-service";
+import Skeleton from "react-loading-skeleton";
 
 const ImageViewer = ({ image }) => {
   return (
@@ -47,60 +49,31 @@ const ImageViewer = ({ image }) => {
   );
 };
 
-const WTColumn = ({ images }) => {
-  const [selected, setSelected] = useState(0);
-  return (
-    <Row className={`mt-3 ${styles.images}`}>
-      <Col xs={12} style={{ display: "flex" }}>
-        <ImageViewer image={images?.[selected]?.jpegUrl} />
-      </Col>
-      {images?.map((image, i) => (
-        <Col md={4} lg={3} className="mb-2">
-          <div
-            style={{
-              minHeight: 50,
-              backgroundColor: "#ddd",
-              borderRadius: 5,
-              overflow: "hidden",
-              opacity: i === selected ? 0.5 : 1,
-            }}
-            onClick={() => {
-              setSelected(i);
-            }}
-          >
-            // TODO: decode url if it's base64
-            <img src={image.thumbnailUrl} style={{ width: "100%" }} />
-          </div>
-        </Col>
-      ))}
-    </Row>
-  );
-};
-
 const Column = ({ images }) => {
   const [selected, setSelected] = useState(0);
   return (
     <Row className={`mt-3 ${styles.images}`}>
       <Col xs={12} style={{ display: "flex" }}>
-        {!!images?.[selected]?.fullFileUrl && (
-          <ImageViewer image={images?.[selected]?.fullFileUrl} />
+        {!!images?.[selected]?.jpegUrl && (
+          <ImageViewer image={images?.[selected]?.jpegUrl} />
         )}
       </Col>
       {images?.map((image, i) => (
-        <Col md={4} lg={3} className="mb-2">
+        <Col key={image.observationId} md={4} lg={3} className="mb-2">
           <div
             style={{
               minHeight: 50,
               backgroundColor: "#ddd",
               borderRadius: 5,
               overflow: "hidden",
-              opacity: i === selected ? 0.5 : 1,
+              border: 'solid #B65A15',
+              borderWidth: i === selected ? 2 : 0,
             }}
             onClick={() => {
               setSelected(i);
             }}
           >
-            <img src={image.thumbnailUrl} style={{ width: "100%" }} />
+            <img src={image.thumbnailUrl} style={{ width: "100%" }} alt="" />
           </div>
         </Col>
       ))}
@@ -109,23 +82,32 @@ const Column = ({ images }) => {
 };
 
 const ImagesCompare = () => {
-  const [wt, setWt] = useState(null);
-  useEffect(() => setWt(gData), []);
   const router = useRouter();
-  const { parameterName = "", pid } = router.query;
-  const { data: images } = useQuery({
-    queryKey: ['genes', pid, 'images', parameterName],
-    queryFn: () => fetchAPI(`/api/imaging/v1/thumbnails?genotype=PMAV`),
+  const { parameterStableId = "", pid } = router.query;
+  const { data: mutantImages } = useQuery({
+    queryKey: ['genes', pid, 'images', parameterStableId],
+    queryFn: () => fetchAPI(`/api/v1/images/find_by_mgi_and_stable_id?mgiGeneAccessionId=${pid}&parameterStableId=${parameterStableId}`),
     enabled: router.isReady,
-    placeholderData: [],
-  })
+    select: data => {
+      const selectedDataset = data.find(d => d.pipelineStableId.includes('IMPC'));
+      return !!selectedDataset ? selectedDataset : data[0];
+    }
+  });
 
-  let procedureName;
-  let wtImages = []
-  if (wt?.geneImages) {
-    const groups = _.groupBy(wt.geneImages, "parameterName");
-    wtImages = groups?.[parameterName as string];
-    procedureName = wtImages?.[0].procedureName;
+  const { data: controlImages } = useQuery({
+    queryKey: ['genes', pid, 'images', parameterStableId, 'control'],
+    queryFn: () => fetchAPI(`/api/v1/images/find_by_stable_id_and_sample_id?biologicalSampleGroup=control&parameterStableId=${parameterStableId}`),
+    enabled: router.isReady && !!mutantImages?.strainAccessionId,
+    select: data => data.find(d => d.strainAccessionId === mutantImages.strainAccessionId)
+  });
+
+  const [selectedSex, setSelectedSex] = useState('both');
+  const [selectedZyg, setSelectedZyg] = useState('both');
+
+  const filterImages = (images) => {
+    return images
+      .filter(i => selectedSex !== 'both' ? i.sex === selectedSex : true)
+      .filter(i => selectedZyg !== 'both' ? i.zygosity === selectedZyg : true)
   }
 
   return <>
@@ -139,23 +121,47 @@ const ImagesCompare = () => {
         </Link>
         <p className={styles.subheading}>Images</p>
         <h1 className="mb-4 mt-2">
-          <strong>{procedureName}</strong> / {parameterName}
+          <strong>{mutantImages?.procedureName || <Skeleton />}</strong> / {mutantImages?.parameterName || <Skeleton />}
         </h1>
+        <div className={`mb-4 ${styles.filtersWrapper}`}>
+          Show by:
+          <div className={styles.filter}>
+            <strong>Sex:</strong>
+            <Badge className={styles.badge} pill bg="secondary" onClick={() => setSelectedSex('both')}>
+              Both
+              <FontAwesomeIcon icon={faMarsAndVenus} />
+            </Badge>
+            <Badge className={styles.badge} pill bg="secondary" onClick={() => setSelectedSex('female')}>
+              Female
+              <FontAwesomeIcon icon={faVenus} />
+            </Badge>
+            <Badge className={styles.badge} pill bg="secondary" onClick={() => setSelectedSex('male')}>
+              Male
+              <FontAwesomeIcon icon={faMars} />
+            </Badge>
+          </div>
+          <div className={styles.filter}>
+            <strong>Zygosity:</strong>
+            <Badge className={styles.badge} pill bg="secondary" onClick={() => setSelectedZyg('both')}>
+              Both
+            </Badge>
+            <Badge className={styles.badge} pill bg="secondary" onClick={() => setSelectedZyg('heterozygote')}>
+              Het.
+            </Badge>
+            <Badge className={styles.badge} pill bg="secondary" onClick={() => setSelectedZyg('homozygote')}>
+              Hom.
+            </Badge>
+          </div>
+        </div>
         <div>
           <Row>
             <Col sm={6}>
               <h3>WT Images</h3>
-              {wtImages?.length > 0 ? (
-                <WTColumn images={wtImages} />
-              ) : (
-                <Alert variant="primary">
-                  There are no images for this procedure.
-                </Alert>
-              )}
+              <Column images={filterImages(controlImages?.images)} />
             </Col>
             <Col sm={6}>
               <h3>Mutant Images</h3>
-              <Column images={images} />
+              <Column images={filterImages(mutantImages?.images)} />
             </Col>
           </Row>
         </div>
