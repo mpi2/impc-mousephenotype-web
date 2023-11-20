4742e3347dc04dbaa05996eb98aa29046a726dbd
Francisco Peña <francisco@ebi.ac.uk>
2023-11-20 14:36:30 +0000

    set Pagination default range as a single page

--- components/Data/DataComparison/index.tsx
+++ components/Data/DataComparison/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Pagination from "../../Pagination";
 import SortableTable from "../../SortableTable";
 import _ from "lodash";
