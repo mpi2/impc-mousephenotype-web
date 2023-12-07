7af0a51ef282a28e2d8c18f67f4e555c38096b43
Francisco Peña <francisco@ebi.ac.uk>
2023-12-07 21:22:42 +0000

    add generic table component with pagination and filtering

---
+++ components/SmartTable/index.tsx
@@ -0,0 +1,3 @@
+
+export { default as SmartTable } from './SmartTable';
+export { default as SimpleTextCell } from './Cells/SimpleText';
--- shared/models/index.ts
+++ shared/models/index.ts
@@ -1,1 +1,11 @@
+import * as Gene from './gene';
+import * as Phenotype from './phenotype';
+import type { PhenotypeRef } from './phenotype-ref';
+
 export type { PhenotypeRef } from './phenotype-ref';
+export type { TableCellProps } from './TableCell'
+export type { Gene };
+export type { Phenotype };
+
+type Model = PhenotypeRef | typeof Gene | typeof Phenotype;
+export type { Model };
