6f50b4a5ced40eb97e1b7f9699220d371119b51a
Francisco Peña <francisco@ebi.ac.uk>
2023-12-12 18:08:00 +0000

    renamed TransformCell to OptionsCell

--- components/SmartTable/Cells/Options.tsx
+++ components/SmartTable/Cells/Options.tsx
@@ -4,8 +4,8 @@ import _ from "lodash";
 type Props = {
   options: Record<string, string>;
 }
-const LinkCell = <T extends Model>(props: TableCellProps<T> & Props) => {
+const OptionsCell = <T extends Model>(props: TableCellProps<T> & Props) => {
   return <span>{props.options[_.get(props.value, props.field) as string]}</span>
 };
 
-export default LinkCell;
+export default OptionsCell;
--- components/SmartTable/index.tsx
+++ components/SmartTable/index.tsx
@@ -2,4 +2,4 @@
 export { default as SmartTable } from './SmartTable';
 export { default as SimpleTextCell } from './Cells/SimpleText';
 export { default as LinkCell } from './Cells/Link';
-export { default as TransformCell } from './Cells/Transform';
+export { default as OptionsCell } from './Cells/Options';
