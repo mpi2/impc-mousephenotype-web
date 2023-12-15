8dc867b152955a7ed48c7ddae2c8c1a337b6c992
Francisco Peña <francisco@ebi.ac.uk>
2023-12-15 13:25:32 +0000

    update AlleleCell to use style prop

--- components/SmartTable/Cells/Allele.tsx
+++ components/SmartTable/Cells/Allele.tsx
@@ -5,10 +5,10 @@ import { formatAlleleSymbol } from "@/utils";
 const AlleleCell = <T extends Model>(props: TableCellProps<T>) => {
   const allele = formatAlleleSymbol(_.get(props.value, props.field) as string);
   return (
-    <>
+    <span style={props.style}>
       {allele[0]}
       <sup>{allele[1]}</sup>
-    </>
+    </span>
   )
 };
 
