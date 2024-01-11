347594cd90144011f0cc93969ce5a470f1fba03f
Francisco Peña <francisco@ebi.ac.uk>
2024-01-11 12:07:21 +0000

    add useGeneExternalLinksQuery hook

---
+++ shared/hooks/gene-external-links.query.ts
@@ -0,0 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchAPI } from "@/api-service";
+import _ from 'lodash';
+
+type ExternalLink = {
+  href: string;
+  label: string;
+  providerName: string;
+}
+
+export const useGeneExternalLinksQuery = (mgiGeneAccessionId: string, routerIsReady: boolean) => {
+  const { data: providers } = useQuery({
+    queryKey: ['external-links-providers'],
+    queryFn: () => fetchAPI(`/api/v1/genes/gene_external_links/providers`),
+    select: providerList => providerList.reduce((acc, provider) => {
+      acc[provider.providerName] = provider.providerDescription;
+      return acc;
+    }, {}),
+    placeholderData: {}
+  });
+
+  const hasLoadedProvidersData = !!providers && Object.keys(providers).length > 0;
+  return useQuery({
+    queryKey: ['gene', mgiGeneAccessionId, 'external-links'],
+    queryFn: () => fetchAPI(`/api/v1/genes/${mgiGeneAccessionId}/gene_external_links`),
+    enabled: routerIsReady && hasLoadedProvidersData,
+    select: (data) => {
+      const linkList: Array<ExternalLink> = Array.isArray(data) ? data : [data];
+      const linksByProvider = _.groupBy(linkList, link => link.providerName);
+      return Object.entries(linksByProvider).map(([providerName, links]) => ({
+        providerName,
+        providerDescription: providers[providerName],
+        links,
+      }))
+    },
+    placeholderData: []
+  });
+}
--- shared/hooks/index.ts
+++ shared/hooks/index.ts
@@ -3,4 +3,5 @@ export { useGeneAssociationsQuery } from './gene-associations.query';
 export { useSignificantPhenotypesQuery } from './significant-phenotypes.query';
 export { useViabilityQuery } from './viability.query';
 export { useGeneExpressionQuery } from './gene-expression.query';
+export { useGeneExternalLinksQuery } from './gene-external-links.query';
 
