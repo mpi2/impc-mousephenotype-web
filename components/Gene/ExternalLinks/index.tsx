1a739b9446aec8ad58fbff4030035b00f31b2533
Francisco Peña <francisco@ebi.ac.uk>
2024-01-12 14:30:40 +0000

    finished 1st version of external links layout

--- components/Gene/ExternalLinks/index.tsx
+++ components/Gene/ExternalLinks/index.tsx
@@ -1,26 +1,46 @@
 import { Card } from "@/components";
 import { useRouter } from "next/router";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { GeneContext } from "@/contexts";
 import { Col, Container, Row } from "react-bootstrap";
 import { useGeneExternalLinksQuery } from "@/hooks";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 
 
 const ExternalLinks = () => {
   const router = useRouter();
   const gene = useContext(GeneContext);
 
-  const { data } = useGeneExternalLinksQuery(gene.mgiGeneAccessionId, router.isReady);
+  const { data: providers } = useGeneExternalLinksQuery(gene.mgiGeneAccessionId, router.isReady);
 
-  console.log(data);
 
   return (
     <Card>
       <h2>External links</h2>
       <Container>
         <Row>
-          <Col></Col>
-          <Col></Col>
+          {providers.map(provider => (
+            <Col className="mb-3" xs={6} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly' }}>
+              <div>
+                <b>{provider.providerName}</b>&nbsp;-&nbsp;
+                <span style={{fontSize: '80%'}}>{provider.providerDescription}</span>
+              </div>
+              <ul style={{marginBottom: 0, display: 'flex', columnGap: '1.3em'}}>
+                {provider.links.map(link => (
+                  <a className="primary link" href={link.href}>
+                    {link.label}
+                    &nbsp;
+                    <FontAwesomeIcon
+                      icon={faExternalLinkAlt}
+                      className="grey"
+                      size="xs"
+                    />
+                  </a>
+                ))}
+              </ul>
+            </Col>
+          ))}
         </Row>
       </Container>
     </Card>
