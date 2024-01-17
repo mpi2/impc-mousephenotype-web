10b685a993459279e3c2dd44920c632e7dbb7069
Francisco Peña <francisco@ebi.ac.uk>
2024-01-17 14:15:14 +0000

    add test env file and load it from jest setup file

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,9 @@
 import '@testing-library/jest-dom';
 import { server } from "./mocks/server";
+import { loadEnvConfig } from '@next/env';
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
