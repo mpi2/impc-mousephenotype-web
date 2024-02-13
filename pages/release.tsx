39132d60aecaed1eb29887237f301e582e097673
Francisco Peña <francisco@ebi.ac.uk>
2024-02-13 10:54:55 +0000

    updated release page

--- pages/release.tsx
+++ pages/release.tsx
@@ -4,16 +4,191 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import Markdown from 'react-markdown'
+import mockData from '../mocks/data/release/release_metadata.json';
+import { NumberCell, PlainTextCell, SmartTable } from "@/components/SmartTable";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
-const valuePair = (key, value) => (
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+type SampleCounts = {
+  phenotypingCentre: string;
+  mutantLines: number;
+  mutantSpecimens: number;
+  controlSpecimens: number;
+};
+
+type DataQualityCheck = {
+  dataType: string;
+  count: number;
+}
+
+const valuePair = (key: string, value: string | number) => (
   <div>
     <span className="grey">{key}: </span>
-    <strong>{value}</strong>
+    {typeof value === 'number' ? (
+      <strong>{value.toLocaleString()}</strong>
+    ) : (
+      <strong>{value}</strong>
+    )}
   </div>
 );
 
 const ReleaseNote = () => {
   const [showAll, setShowAll] = useState(false);
+
+  const formatDate = (date: string) => {
+    const dateObj = new Date(date);
+    return dateObj.toLocaleDateString('en-GB', { year: 'numeric', month: 'long', day: 'numeric'});
+  };
+  const findZygosityCount = (p, zygosity) => {
+    return p.counts.find(count => count.zygosity === zygosity)?.count || 0;
+  };
+  const getProductionStatusByType = (statusArray, type: string) => {
+    return statusArray.find(s => s.statusType === type)
+      .counts
+      .filter(count => count.status !== null);
+  };
+
+  const getProcedureCount = (procedure, type: 'Early adult'|'Late adult'|'Embryo') : number => {
+    const embryoLifeStages = ['Embryo', 'E18.5', 'E9.5', 'E15.5', 'E12.5']
+    switch (type) {
+      case "Early adult":
+      case "Late adult":
+        return procedure.counts.find(c => c.lifeStage === type)?.count || 0;
+      case "Embryo":
+        return procedure.counts.find(c => embryoLifeStages.includes(c.lifeStage))?.count || 0;
+    }
+  }
+
+  const phenotypeAssociationsOpts = {
+    maintainAspectRatio: true,
+    scales: {
+      x: {
+        stacked: true,
+      },
+      y: {
+        stacked: true,
+        title: { display: true, text: 'Number of genotype-phenotype associations' }
+      },
+    },
+    plugins: {
+      legend: {
+        position: 'bottom' as const
+      }
+    }
+  };
+
+  const phenotypeAssociationsData = {
+    labels: mockData.phenotypeAnnotations.map(phenotype => phenotype.topLevelPhenotype),
+    datasets: [
+      {
+        label: 'Homozygote',
+        data: mockData.phenotypeAnnotations.map(p => findZygosityCount(p, 'homozygote')),
+        backgroundColor: 'rgb(119, 119, 119)'
+      },
+      {
+        label: 'Heterozygote',
+        data: mockData.phenotypeAnnotations.map(p => findZygosityCount(p, 'heterozygote')),
+        backgroundColor: 'rgb(9, 120, 161)',
+      },
+      {
+        label: 'Hemizygote',
+        data: mockData.phenotypeAnnotations.map(p => findZygosityCount(p, 'hemizygote')),
+        backgroundColor: 'rgb(239, 123, 11)',
+      }
+    ]
+  };
+
+  const overallProdStatusOptions = {
+    scales: {
+      y: { title: { display: true, text: 'Number of genes' } }
+    },
+    plugins: {
+      legend: {
+        display: false,
+      }
+    }
+  };
+
+  const genotypingStatusChartData = {
+    labels: getProductionStatusByType(mockData.productionStatus, 'genotyping').map(c => {
+      return c.status === 'Mouse Allele Modification Genotype Confirmed' ? 'Genotype Confirmed' : c.status
+    }),
+    datasets: [
+      {
+        label: '',
+        data: getProductionStatusByType(mockData.productionStatus, 'genotyping').map(c => c.count),
+        backgroundColor: 'rgb(239, 123, 11)'
+      }
+    ]
+  };
+
+  const phenotypingStatusChartData = {
+    labels: getProductionStatusByType(mockData.productionStatus, 'phenotyping').map(c => c.status),
+    datasets: [
+      {
+        label: '',
+        data: getProductionStatusByType(mockData.productionStatus, 'phenotyping').map(c => c.count),
+        backgroundColor: 'rgb(239, 123, 11)'
+      }
+    ]
+  };
+
+  const phenotypeCallsByProdOpts = {
+    maintainAspectRatio: true,
+    scales: {
+      x: {
+        stacked: true,
+      },
+      y: {
+        stacked: true,
+        title: { display: true, text: 'Number of phenotype calls' }
+      },
+    },
+    plugins: {
+      legend: {
+        position: 'bottom' as const
+      }
+    }
+  };
+
+  const phenotypeCallsByProdData = {
+    labels: mockData.phenotypeAssociationsByProcedure.map(phenotype => phenotype['procedure_name']),
+    datasets: [
+      {
+        label: 'Early Adult',
+        data: mockData.phenotypeAssociationsByProcedure.map(p => getProcedureCount(p, 'Early adult')),
+        backgroundColor: 'rgb(119, 119, 119)'
+      },
+      {
+        label: 'Late Adult',
+        data: mockData.phenotypeAssociationsByProcedure.map(p => getProcedureCount(p, 'Late adult')),
+        backgroundColor: 'rgb(9, 120, 161)',
+      },
+      {
+        label: 'Embryo',
+        data: mockData.phenotypeAssociationsByProcedure.map(p => getProcedureCount(p, 'Embryo')),
+        backgroundColor: 'rgb(239, 123, 11)',
+      }
+    ]
+  };
 
   return (
     <>
@@ -26,21 +201,21 @@ const ReleaseNote = () => {
           >
             RELEASE NOTES
           </p>
-          <h1 className="h1 mb-2">IMPC Data Release 19.0 Notes</h1>
-          <p className="grey mb-3">9th May 2023</p>
+          <h1 className="h1 mb-2">IMPC Data Release {mockData.dataReleaseVersion} Notes</h1>
+          <p className="grey mb-3">{formatDate(mockData.dataReleaseDate)}</p>
 
           <Row className="mb-4">
             <Col lg={6}>
               <h3 className="mb-0 mt-3 mb-2">Summary</h3>
-              {valuePair("Number of phenotyped genes", "8,481")}
-              {valuePair("Number of phenotyped mutant lines", "9,138")}
-              {valuePair("Number of phenotype calls", "102,805")}
+              {valuePair("Number of phenotyped genes", mockData.summaryCounts.phenotypedGenes)}
+              {valuePair("Number of phenotyped mutant lines", mockData.summaryCounts.phenotypedLines)}
+              {valuePair("Number of phenotype calls", mockData.summaryCounts.phentoypeCalls)}
             </Col>
             <Col lg={6}>
               <h3 className="mb-0 mt-3 mb-2">Data access</h3>
               <div className="mb-1">
-                <a href="#" className="link orange-dark ">
-                  Ftp interface{" "}
+                <a href="https://www.mousephenotype.org/help/non-programmatic-data-access" className="link orange-dark ">
+                  Ftp interface&nbsp;
                   <FontAwesomeIcon
                     icon={faExternalLink}
                     size="sm"
@@ -49,8 +224,8 @@ const ReleaseNote = () => {
                 </a>
               </div>
               <div>
-                <a href="#" className="link orange-dark ">
-                  RESTful interfaces{" "}
+                <a href="https://www.mousephenotype.org/help/programmatic-data-access" className="link orange-dark">
+                  RESTful interfaces&nbsp;
                   <FontAwesomeIcon
                     icon={faExternalLink}
                     size="sm"
@@ -61,42 +236,58 @@ const ReleaseNote = () => {
             </Col>
           </Row>
 
-          {valuePair("Statistical package", "OpenStats (1.12.0)")}
-          {valuePair("Genome Assembly", "Mus musculus (GRCm38)")}
+          {valuePair("Statistical package", `${mockData.statisticalAnalysisPackage.name} (${mockData.statisticalAnalysisPackage.version})`)}
+          {valuePair("Genome Assembly", `${mockData.genomeAssembly.species} (${mockData.genomeAssembly.version})`)}
 
           <h3 className="mb-0 mt-5 mb-2">Highlights</h3>
-          <p>Includes approximately 2.1 million more data points.</p>
+          <p>
+            <Markdown>{mockData.dataReleaseNotes}</Markdown>
+          </p>
         </Card>
         <Card>
-          <h2>Total number of lines and specimens in DR 19.0</h2>
-          <p className="grey">Insert table</p>
+          <h2>Total number of lines and specimens in DR {mockData.dataReleaseVersion}</h2>
+          <SmartTable<SampleCounts>
+            data={mockData.sampleCounts}
+            defaultSort={['phenotypingCentre', 'asc']}
+            columns={[
+              {width: 1, label: 'Phenotyping Center', field: 'phenotypingCentre', cmp: <NumberCell /> },
+              {width: 1, label: 'Mutant Lines', field: 'mutantLines', cmp: <NumberCell /> },
+              {width: 1, label: 'Baseline Mice', field: 'controlSpecimens', cmp: <NumberCell /> },
+              {width: 1, label: 'Mutant Mice', field: 'mutantSpecimens', cmp: <NumberCell /> }
+            ]}
+          />
         </Card>
         <Card>
           <h2>Experimental data and quality checks</h2>
-          <p className="grey">Insert table</p>
+          <SmartTable<DataQualityCheck>
+            data={mockData.dataQualityChecks}
+            defaultSort={['count', 'desc']}
+            columns={[
+              {width: 1, label: 'Data Type', field: 'dataType', cmp: <PlainTextCell /> },
+              {width: 1, label: 'QC Passed Data Points', field: 'count', cmp: <NumberCell /> },
+            ]}
+          />
+          <span className="small">* Excluded from statistical analysis.</span>
         </Card>
         <Card>
           <h2>Distribution of phenotype annotations</h2>
-          <p className="grey">Insert Chart</p>
+          <div style={{ position: 'relative', height: '600px' }}>
+            <Bar options={phenotypeAssociationsOpts} data={phenotypeAssociationsData} />
+          </div>
         </Card>
         <Card>
           <h2>Production status</h2>
           <h3>Overall</h3>
           <Row className="mb-4">
             <Col lg={6}>
-              <p className="grey">Insert Chart</p>
+              <div style={{ position: 'relative', height: '400px' }}>
+                <Bar options={overallProdStatusOptions} data={genotypingStatusChartData} />
+              </div>
             </Col>
             <Col lg={6}>
-              <p className="grey">Insert Chart</p>
-            </Col>
-          </Row>
-          <h3>By center</h3>
-          <Row className="mb-4">
-            <Col lg={6}>
-              <p className="grey">Insert Chart</p>
-            </Col>
-            <Col lg={6}>
-              <p className="grey">Insert Chart</p>
+              <div style={{position: 'relative', height: '400px'}}>
+                <Bar options={overallProdStatusOptions} data={phenotypingStatusChartData}/>
+              </div>
             </Col>
           </Row>
           <p>
@@ -106,12 +297,9 @@ const ReleaseNote = () => {
         </Card>
         <Card>
           <h2>Phenotype associations</h2>
-          <p className="grey">Insert Chart</p>
-        </Card>
-        <Card>
-          <h2>Trends</h2>
-          <p className="grey">Insert Chart</p>
-          <p className="grey">Insert Chart</p>
+          <div style={{position: 'relative', height: '600px'}}>
+            <Bar options={phenotypeCallsByProdOpts} data={phenotypeCallsByProdData}/>
+          </div>
         </Card>
 
         <Card>
