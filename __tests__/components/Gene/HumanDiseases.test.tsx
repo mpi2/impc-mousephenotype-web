0e0b5c4ad5991ff6a9e24d30eb5264551a901e3f
Francisco Peña <francisco@ebi.ac.uk>
2024-03-18 15:23:46 +0000

    fix: HumanDiseases test and add mock response for successful request

--- __tests__/components/Gene/HumanDiseases.test.tsx
+++ __tests__/components/Gene/HumanDiseases.test.tsx
@@ -5,18 +5,23 @@ import mockRouter from "next-router-mock";
 import { server } from "../../../mocks/server";
 import { rest } from 'msw';
 import userEvent from "@testing-library/user-event";
+import otogData from '../../../mocks/data/genes/MGI:1202064/disease.json';
 
 jest.mock('next/router', () => jest.requireActual('next-router-mock'));
 
 describe('Gene human diseases component', () => {
   it('should display information', async () => {
     // misuse of query param :) to pass param to fetch function
+    server.use(
+      rest.get(`${API_URL}/api/v1/genes/MGI:1202064/disease`, (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json(otogData));
+      })
+    );
     await mockRouter.push('/genes/MGI:1202064?pid=MGI:1202064');
     renderWithClient(<GeneHumanDiseases gene={{ geneSymbol: 'Otog' }} />);
-    expect(screen.getByRole('heading')).toHaveTextContent('Human diseases caused by Otog mutations');
-    expect(await screen.findByRole('alert')).toBeInTheDocument();
-    const assocDiseasesTab = screen.getByRole('tab', { name: /Human diseases associated/ });
-    const predictedDiseasesTab = screen.getByRole('tab', { name: /Human diseases predicted/ });
+    expect(await screen.findByRole('heading')).toHaveTextContent('Human diseases caused by Otog mutations');
+    const assocDiseasesTab = await screen.findByRole('tab', { name: /Human diseases associated/ });
+    const predictedDiseasesTab = await screen.findByRole('tab', { name: /Human diseases predicted/ });
     expect(assocDiseasesTab).toBeInTheDocument();
     expect(predictedDiseasesTab).toBeInTheDocument();
   });
@@ -40,7 +45,7 @@ describe('Gene human diseases component', () => {
       rest.get(`${API_URL}/api/v1/genes/MGI:1922546/disease`, (req, res, ctx) => {
         return res(ctx.status(500));
       })
-    )
+    );
     await mockRouter.push('/genes/MGI:1922546?pid=MGI:1922546');
     renderWithClient(<GeneHumanDiseases gene={{ geneSymbol: 'Cep43' }} />);
     expect(await screen.findByRole('alert')).toBeInTheDocument();
