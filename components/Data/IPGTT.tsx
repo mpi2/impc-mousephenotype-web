388bfdec8559a4d659780d6305439e7910c1bc3c
Francisco Peña <francisco@ebi.ac.uk>
2024-03-28 11:00:19 +0000

    add IPGTT chart, update ABR to use new hook

---
+++ components/Data/IPGTT.tsx
@@ -0,0 +1,47 @@
+import { Dataset } from "@/models";
+import { Tab, Tabs } from "react-bootstrap";
+import { useRelatedParametersQuery } from "@/hooks/related-parameters.query";
+import { useState } from "react";
+import { getChartType } from "@/components/Data/Utils";
+
+const parameterList = [
+  "IMPC_IPG_001_001",
+  "IMPC_IPG_002_001",
+  "IMPC_IPG_010_001",
+  "IMPC_IPG_011_001",
+  "IMPC_IPG_012_001",
+];
+
+type IPGTTProps = {
+  datasetSummaries: Array<Dataset>;
+  onNewSummariesFetched: (missingSummaries: Array<any>) => void;
+};
+
+const IPGTT = (props: IPGTTProps) => {
+  const { datasetSummaries, onNewSummariesFetched } = props;
+  const [key, setKey] = useState(datasetSummaries[0].parameterName);
+
+  const datasets = useRelatedParametersQuery(
+    datasetSummaries,
+    parameterList,
+    onNewSummariesFetched
+  );
+
+  return (
+    <>
+      <Tabs
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+      >
+        {datasets.map(dataset => (
+          <Tab title={dataset.parameterName} eventKey={dataset.parameterName}>
+            {getChartType(dataset, key === dataset.parameterName)}
+          </Tab>
+        ))}
+      </Tabs>
+    </>
+  );
+
+};
+
+export default IPGTT;
