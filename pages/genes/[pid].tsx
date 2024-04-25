e2b49cc4edf5142e540db05d6433645e06c745fe
Francisco Peña <francisco@ebi.ac.uk>
2024-04-25 11:13:57 +0100

    update gene page to fetch summary from server and generate all static paths

--- pages/genes/[pid].tsx
+++ pages/genes/[pid].tsx
@@ -15,6 +15,9 @@ import dynamic from "next/dynamic";
 import { AllelesStudiedContext, GeneContext, NumAllelesContext } from "@/contexts";
 import { useGeneSummaryQuery } from "@/hooks";
 import Head from "next/head";
+import { GeneSummary } from "@/models/gene";
+import { fetchAPI } from "@/api-service";
+import geneList from '../../mocks/data/all_genes_list.json';
 
 const HumanDiseases = dynamic(
   () => import("@/components/Gene/HumanDiseases"),
@@ -23,19 +26,21 @@ const HumanDiseases = dynamic(
   }
 );
 
-const Gene = () => {
+type GenePageProps = {
+  gene: GeneSummary;
+}
+
+const Gene = (props: GenePageProps) => {
+  const { gene: geneFromServer } = props;
   const router = useRouter();
   const [numOfAlleles, setNumOfAlleles] = useState<number>(null);
   const [allelesStudied, setAlleles] = useState<Array<string>>([]);
   const numAllelesContextValue = {numOfAlleles, setNumOfAlleles};
   const allelesStudiedContextValue = {allelesStudied, setAlleles};
 
-  const {
-    isLoading,
-    isError,
-    data: gene,
-    error
-  } = useGeneSummaryQuery(router.query.pid as string, router.isReady);
+  const {data: gene} = useGeneSummaryQuery(router.query.pid as string, router.isReady && !geneFromServer, geneFromServer);
+
+  const geneData = geneFromServer || gene;
 
   useEffect(() => {
     if (gene) {
@@ -46,32 +51,30 @@ const Gene = () => {
         }, 500);
       }
     }
-  }, [gene]);
+  }, [geneData]);
+
+
 
   return (
     <>
       <Head>
         <title>{gene?.geneSymbol} Mouse Gene details | International Mouse Phenotyping Consortium</title>
       </Head>
-      <GeneContext.Provider value={gene}>
+      <GeneContext.Provider value={geneData}>
         <NumAllelesContext.Provider value={numAllelesContextValue}>
           <AllelesStudiedContext.Provider value={allelesStudiedContextValue}>
             <GeneComparatorTrigger current={router.query.pid as string} />
             <Search />
             <Container className="page">
-              <Summary {...{ gene, numOfAlleles, loading: isLoading, error: isError ? error.toString(): "" }} />
-              {!!gene && (
-                <>
-                  <Phenotypes gene={gene} />
-                  <Expressions />
-                  <Images gene={gene} />
-                  <HumanDiseases gene={gene} />
-                  <Histopathology />
-                  <Publications gene={gene} />
-                  <ExternalLinks />
-                  <Order allelesStudied={allelesStudied} />
-                </>
-              )}
+              <Summary {...{ gene: geneData, numOfAlleles }} />
+              <Phenotypes gene={geneData} />
+              <Expressions />
+              <Images gene={geneData} />
+              <HumanDiseases gene={geneData} />
+              <Histopathology />
+              <Publications gene={geneData} />
+              <ExternalLinks />
+              <Order allelesStudied={allelesStudied} />
             </Container>
           </AllelesStudiedContext.Provider>
         </NumAllelesContext.Provider>
@@ -79,5 +82,20 @@ const Gene = () => {
     </>
   );
 };
+
+export async function getStaticProps(context) {
+  const { pid: mgiGeneAccessionId } = context.params;
+  const data = await fetchAPI(`/api/v1/genes/${mgiGeneAccessionId}/summary`);
+  return {
+    props: { gene: data }
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = geneList.map(geneAccessionId => ({
+    params: { pid: geneAccessionId }
+  }));
+  return { paths, fallback: true };
+}
 
 export default Gene;
