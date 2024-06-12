85311f497e2a68c1b0329525c85281f382037705
Francisco Peña <francisco@ebi.ac.uk>
2024-06-12 13:37:54 +0100

    updated components snapshots

--- __tests__/components/LandingPages/embryo.test.tsx
+++ __tests__/components/LandingPages/embryo.test.tsx
@@ -2,12 +2,14 @@ import renderer from 'react-test-renderer';
 import EmbryoLandingPage from "@/pages/embryo";
 import { server } from "../../../mocks/server";
 import { rest } from "msw";
-import { API_URL } from "../../utils";
+import { API_URL, createTestQueryClient } from "../../utils";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 jest.mock('next/router', () => jest.requireActual('next-router-mock'));
 
 describe('Embryo landing page', () => {
   it('renders correctly', () => {
+    const client = createTestQueryClient();
     server.use(
       rest.get(`${API_URL}/api/v1/publications`, (req, res, ctx) => {
         return res(ctx.status(200), ctx.json({
@@ -23,7 +25,9 @@ describe('Embryo landing page', () => {
       })
     );
     const tree = renderer.create(
-      <EmbryoLandingPage />
+      <QueryClientProvider client={client}>
+        <EmbryoLandingPage />
+      </QueryClientProvider>
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
