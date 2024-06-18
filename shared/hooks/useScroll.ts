ace1e5c8e9641f8761d000dca4ebe4d082684e00
Francisco Peña <francisco@ebi.ac.uk>
2024-06-18 17:00:20 +0100

    add back to top button, show only when user has scrolled down and scrolls up

--- shared/hooks/useScroll.ts
+++ shared/hooks/useScroll.ts
@@ -1,7 +1,10 @@
 import { useLayoutEffect, useState } from "react";
 
+let scrollPos = 0;
 export const useScroll = () => {
-  const [state, setState] = useState({ x: 0, y: 0, perX: 0, perY: 0 });
+  const [state, setState] = useState(
+    { x: 0, y: 0, perX: 0, perY: 0, scrollDirection: null }
+  );
 
   useLayoutEffect(() => {
     const handleScroll = () => {
@@ -10,7 +13,9 @@ export const useScroll = () => {
         y: window.scrollY,
         perX: Math.round((window.scrollX / window.innerWidth) * 100),
         perY: Math.round((window.scrollY / window.innerHeight) * 100),
+        scrollDirection: window.scrollY > scrollPos ? "DOWN" : "UP",
       });
+      scrollPos = window.scrollY;
     };
     handleScroll();
     window.addEventListener("scroll", handleScroll);
