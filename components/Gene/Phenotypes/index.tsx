7ec0b150c1cd5eb0de3175fbb34cd259af6c3829
Francisco Peña <francisco@ebi.ac.uk>
2024-06-20 15:40:10 +0100

    added help text for Phenotype section

--- components/Gene/Phenotypes/index.tsx
+++ components/Gene/Phenotypes/index.tsx
@@ -1,4 +1,4 @@
-import { Alert, Spinner, Tab, Tabs } from "react-bootstrap";
+import { Alert, Col, Row, Spinner, Tab, Tabs } from "react-bootstrap";
 import Card from "../../Card";
 import AllData from "./AllData";
 import SignificantPhenotypes from "./SignificantPhenotypes";
@@ -14,6 +14,9 @@ import { orderPhenotypedSelectionChannel, summarySystemSelectionChannel } from "
 import _ from 'lodash';
 import { AllelesStudiedContext } from "@/contexts";
 import { Variant } from "react-bootstrap/types";
+import { SectionHeader } from "@/components";
+import { faTriangleExclamation, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 const StatisticalAnalysis = dynamic(
@@ -140,8 +143,48 @@ const Phenotypes = ({ gene }: { gene: GeneSummary }) => {
   }
 
   return (
-    <Card id="data">
-      <h2>Phenotypes</h2>
+    <Card id="data" style={{ position: 'relative' }}>
+      <SectionHeader
+        containerId="#data"
+        title="Phenotypes"
+      >
+        <Row className="mb-4">
+          <p>
+            The Phenotypes section has three tabs. Each tab provides different views of the data, emphasising different aspects, so users can quickly find what they are looking for.
+          </p>
+          <h3>Significant phenotypes table</h3>
+          <p>
+            The significant phenotypes table shows significant genotype-phenotype effects as identified by the IMPC applying dedicated phenotyping tests and statistical procedures.
+            Phenotypes in the mutant mice that differ from those of the controls are annotated using a Mammalian Phenotype (MP) Ontology term an ontology designed to describe abnormalities in mouse strains.
+          </p>
+        </Row>
+        <Row className="mb-4">
+          <Col xs={2} style={{ textAlign: "center" }}>
+            <FontAwesomeIcon icon={faTriangleExclamation} size="3x"/>
+          </Col>
+          <Col>
+            <p>
+              Note that multiple parameters can be associated with the same phenotype, or more than one mouse strain may have been phenotyped.
+              Thus, when multiple associations are established with one phenotype, only the most sigificant association (P value) is shown in this table.
+            </p>
+          </Col>
+        </Row>
+        <Row className="mb-4">
+          <h3>Graphical Analysis</h3>
+          <p>
+            Here the most significant result for each parameter is displayed.
+            Results can be grouped by physiological system or procedure.
+            Hovering over the points shows measurement-related information.
+            Clicking on the points displays the underlying data by opening up the chart page.
+          </p>
+        </Row>
+        <Row className="mb-4">
+          <h3>All data table</h3>
+          <p>
+            The All data table displays all data available for all knockout mouse lines associated with a gene.
+          </p>
+        </Row>
+      </SectionHeader>
       <Tabs
         activeKey={tabKey}
         onSelect={key => setTabKey(key)}
