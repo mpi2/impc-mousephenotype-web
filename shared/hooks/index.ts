9d2e354a638a8d2611c1185bc23478456ec2460a
Francisco Peña <francisco@ebi.ac.uk>
2024-06-24 17:11:19 +0100

    Graphical Analysis tab, separate component and chart into different files

--- shared/hooks/index.ts
+++ shared/hooks/index.ts
@@ -13,3 +13,4 @@ export { useEmbryoWOLQuery } from './embryo-wol.query';
 export { usePagination } from './pagination';
 export { useMultipleS3DatasetsQuery } from './multiple-s3-datasets.query';
 export { useScroll } from './useScroll';
+export { useGeneAllStatisticalResData } from './gene-all-statistical-result-data.query';
