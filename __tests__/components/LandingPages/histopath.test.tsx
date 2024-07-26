17032b13386589321b429cf35ccd3680debc4c85
Francisco Peña <francisco@ebi.ac.uk>
2024-07-26 10:46:36 +0100

    updated test case and snapshot for histopath landing page

--- __tests__/components/LandingPages/histopath.test.tsx
+++ __tests__/components/LandingPages/histopath.test.tsx
@@ -1,12 +1,17 @@
 import { render } from '@testing-library/react';
 import HistopathLandingPage from "@/pages/histopath";
+import { createTestQueryClient } from "../../utils";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 jest.mock('next/router', () => jest.requireActual('next-router-mock'));
 
 describe('Histopathology landing page', () => {
   it('renders correctly', () => {
+    const client = createTestQueryClient();
     const { container } = render(
-      <HistopathLandingPage />
+      <QueryClientProvider client={client}>
+        <HistopathLandingPage />
+      </QueryClientProvider>
     );
     expect(container).toMatchSnapshot();
   });
