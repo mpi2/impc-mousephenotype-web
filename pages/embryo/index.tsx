b00e3f61b86a1ac905959a1f9e27150f6a2f920b
Francisco Peña <francisco@ebi.ac.uk>
2024-07-31 14:30:01 +0100

    feat: update link to embryo vignettes page in embryo landing page

--- pages/embryo/index.tsx
+++ pages/embryo/index.tsx
@@ -13,6 +13,7 @@ import {
   Search,
   SortableTable
 } from "@/components";
+import Link from "next/link";
 
 const EmbryoLandingPage = () => {
   const { data: embryoWOL } = useEmbryoWOLQuery(data => {
@@ -209,7 +210,7 @@ const EmbryoLandingPage = () => {
                     we also provide access to our external partners' embryo images.
                   </li>
                   <li>
-                    <a className="link primary" href="https://beta.mousephenotype.org/data/embryo/vignettes">Embryo Vignettes</a>
+                    <Link className="link primary" href="/embryo/vignettes">Embryo Vignettes</Link>
                     &nbsp;Showcase of best embryo images with detailed explanations.
                   </li>
                   <li>
