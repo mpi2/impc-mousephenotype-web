797a823cca11426cbc35a49a0c304a3b555402fe
Francisco Peña <francisco@ebi.ac.uk>
2024-07-31 12:39:57 +0100

    add: asset prefix based if is running in prod

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   basePath: '/data',
+  assetPrefix: isProd ? 'https://dev.mousephenotype.org/data' : undefined,
   //uncomment the following line when deploying with vercel
   // swcMinify: false,
   async redirects() {
