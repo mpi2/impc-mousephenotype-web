db6f280059c730b56f9779a9055c67e980f2307b
Francisco Peña <francisco@ebi.ac.uk>
2024-08-06 16:02:36 +0100

    fix: remove redirects

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ module.exports = {
   basePath: "/data",
   //uncomment the following line when deploying with vercel
   // swcMinify: false,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/search",
-        permanent: true,
-      },
-    ];
-  },
   // webpack: (config) => {
   //   config.plugins.push(
   //     new webpack.ProvidePlugin({
