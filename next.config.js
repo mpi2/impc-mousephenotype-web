37671d50fc2cbf1eaca123d878a2ef8af7e05f64
Francisco Peña <francisco@ebi.ac.uk>
2024-08-07 11:54:12 +0100

    feat: add redirects

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ module.exports = {
   basePath: "/data",
   //uncomment the following line when deploying with vercel
   // swcMinify: false,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/search",
+        permanent: true,
+      },
+      {
+        source: "/",
+        destination: "/data",
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  },
   // webpack: (config) => {
   //   config.plugins.push(
   //     new webpack.ProvidePlugin({
