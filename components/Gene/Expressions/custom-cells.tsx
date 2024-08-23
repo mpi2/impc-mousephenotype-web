d15fd6b769159eb10633749cc4920daf6ff58cfa
Francisco Peña <francisco@ebi.ac.uk>
2024-08-23 10:23:41 +0100

    feat: include anatomy term in links to image comparator

--- components/Gene/Expressions/custom-cells.tsx
+++ components/Gene/Expressions/custom-cells.tsx
@@ -6,36 +6,43 @@ import { faImage } from "@fortawesome/free-solid-svg-icons";
 import _ from "lodash";
 import { GeneExpressionCounts } from "@/models/gene/expression";
 
-export const ImagesCell = <T extends GeneExpression>(props: TableCellProps<T> & { mgiGeneAccessionId: string; }) => {
+export const ImagesCell = <T extends GeneExpression>(
+  props: TableCellProps<T> & { mgiGeneAccessionId: string }
+) => {
   const imageParameters = props.value.expressionImageParameters;
-  return (
-    !!imageParameters
+  const anatomyTerm = props.value.parameterName;
+  return !!imageParameters
     ? imageParameters.map((p, index) => (
-      <>
-        <Link
-          key={index}
-          className="primary small"
-          href={`/genes/${props.mgiGeneAccessionId}/images/${p.parameterStableId}`}
-        >
-          <FontAwesomeIcon icon={faImage} style={{ marginRight: "0.5rem" }} />
-          {p.parameterName === "lacz images section" ? "Section images" : "Wholemount images"}
-        </Link>
-        <br />
-      </>
-    ))
-    : "n/a"
-  )
-}
-export const ExpressionCell = <T extends GeneExpression>(props: TableCellProps<T> & {
-  expressionRateField: keyof T,
-  countsField: keyof T;
-}) => {
+        <>
+          <Link
+            key={index}
+            className="primary small"
+            href={`/genes/${props.mgiGeneAccessionId}/images/${p.parameterStableId}?anatomyTerm=${anatomyTerm}`}
+          >
+            <FontAwesomeIcon icon={faImage} style={{ marginRight: "0.5rem" }} />
+            {p.parameterName === "lacz images section"
+              ? "Section images"
+              : "Wholemount images"}
+          </Link>
+          <br />
+        </>
+      ))
+    : "n/a";
+};
+export const ExpressionCell = <T extends GeneExpression>(
+  props: TableCellProps<T> & {
+    expressionRateField: keyof T;
+    countsField: keyof T;
+  }
+) => {
   const expressionRate = props.value[props.expressionRateField] as number;
-  const expressionCounts = _.get(props.value, props.countsField) as GeneExpressionCounts;
-  const totalCounts = expressionCounts.expression + expressionCounts.noExpression;
-  return (
-    expressionRate >= 0
-      ? `${expressionRate}% (${expressionCounts.expression}/${totalCounts})`
-      : "n/a"
-  )
-}
+  const expressionCounts = _.get(
+    props.value,
+    props.countsField
+  ) as GeneExpressionCounts;
+  const totalCounts =
+    expressionCounts.expression + expressionCounts.noExpression;
+  return expressionRate >= 0
+    ? `${expressionRate}% (${expressionCounts.expression}/${totalCounts})`
+    : "n/a";
+};
