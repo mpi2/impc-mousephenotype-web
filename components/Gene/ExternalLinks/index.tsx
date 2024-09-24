6608ecf993c2049d641ae7a256762dbfad572cb7
Francisco Peña <francisco@ebi.ac.uk>
2024-09-24 17:14:58 +0100

    feat: display description for each link, update layout based on current website

--- components/Gene/ExternalLinks/index.tsx
+++ components/Gene/ExternalLinks/index.tsx
@@ -8,12 +8,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 import { sectionWithErrorBoundary } from "@/hoc/sectionWithErrorBoundary";
 
-
 const ExternalLinks = () => {
   const router = useRouter();
   const gene = useContext(GeneContext);
 
-  const { data: providers, error, isError } = useGeneExternalLinksQuery(gene.mgiGeneAccessionId, router.isReady);
+  const {
+    data: providers,
+    error,
+    isError,
+  } = useGeneExternalLinksQuery(gene.mgiGeneAccessionId, router.isReady);
 
   if (error || providers?.length === 0) {
     return (
@@ -23,7 +26,7 @@ const ExternalLinks = () => {
           No external links available for <i>{gene.geneSymbol}</i>.
         </Alert>
       </Card>
-    )
+    );
   }
 
   return (
@@ -32,35 +35,50 @@ const ExternalLinks = () => {
       <Container>
         <Row>
           {providers.map((provider, index) => (
-            <Col key={index} className="mb-3" xs={12} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly' }}>
+            <Col
+              key={index}
+              className="mb-3"
+              xs={12}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-evenly",
+              }}
+            >
               <div>
-                <b>{provider.providerName}</b>
-                {!!provider.providerDescription && (
-                  <>
-                    &nbsp;-&nbsp;
-                    <span style={{fontSize: '80%'}}>{provider.providerDescription}</span>
-                  </>
-                )}
-              </div>
-              <ul style={{marginBottom: 0, display: 'flex', columnGap: '1.3em'}}>
-                {provider.links.map(link => (
-                  <a key={link.href} className="primary link" href={link.href} target="_blank">
+                <b>{provider.providerName}</b>&nbsp;-&nbsp;
+                {provider.links.map((link) => (
+                  <a
+                    key={link.href}
+                    className="primary link"
+                    href={link.href}
+                    target="_blank"
+                  >
                     {link.label}
                     <FontAwesomeIcon
                       icon={faExternalLinkAlt}
                       className="grey"
                       size="xs"
-                      style={{ marginLeft: '0.3rem' }}
+                      style={{ marginLeft: "0.3rem" }}
                     />
                   </a>
                 ))}
+              </div>
+              <ul
+                style={{ marginBottom: 0, display: "flex", columnGap: "1.3em" }}
+              >
+                {provider.links.map((link) => link.description)}
               </ul>
             </Col>
           ))}
         </Row>
       </Container>
     </Card>
-  )
-}
+  );
+};
 
-export default sectionWithErrorBoundary(ExternalLinks, 'External links', 'external-links');
+export default sectionWithErrorBoundary(
+  ExternalLinks,
+  "External links",
+  "external-links"
+);
