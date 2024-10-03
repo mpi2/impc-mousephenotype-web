dbfafdf884ba1c1b2212a9618ee602a3fea7600a
Francisco Peña <francisco@ebi.ac.uk>
2024-10-03 12:36:18 +0100

    add: license attribute to gene and phenotype datasets

--- components/Gene/Metadata/metadata.tsx
+++ components/Gene/Metadata/metadata.tsx
@@ -25,6 +25,7 @@ const GeneMetadata = ({ geneSummary }: MetadataProps) => {
     citation: "https://doi.org/10.1093/nar/gkac972",
     isAccessibleForFree: true,
     url: genePageURL,
+    license: "https://creativecommons.org/licenses/by/4.0/",
   };
   return (
     <Head>
