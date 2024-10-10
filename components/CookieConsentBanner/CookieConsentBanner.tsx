1aace62d55eaa1be4d0e9965b2f3deb030371dab
Francisco Peña <francisco@ebi.ac.uk>
2024-10-10 11:55:41 +0100

    feat: basic consent mode implemented
    will load gtag script if user gives consent

--- components/CookieConsentBanner/CookieConsentBanner.tsx
+++ components/CookieConsentBanner/CookieConsentBanner.tsx
@@ -6,22 +6,38 @@ import classNames from "classnames";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCookieBite } from "@fortawesome/free-solid-svg-icons";
 
+export const GOOGLE_TAG_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_ID || "";
+
+const gtag: Gtag.Gtag = function () {
+  (window as any).dataLayer.push(arguments);
+};
+
+// Follows GA basic consent mode
+// https://developers.google.com/tag-platform/security/guides/consent?consentmode=basic
+
 const CookieConsentBanner = () => {
   const bannerRef = useRef<HTMLDivElement>(null);
-  const [bannerPresent, setBannerPresent] = useState<boolean>(true);
   const [bannerVisible, setBannerVisible] = useState<boolean>(false);
   const [bannerHeight, setBannerHeight] = useState<number>(-110);
 
-  useEffect(() => {
-    const cookie = cookieCutter.get("ccbcookie");
-    console.log({ cookie });
-    setBannerPresent(!cookie);
-  }, []);
+  const loadGtagJsScript = () => {
+    gtag("consent", "update", {
+      ad_user_data: "granted",
+      ad_personalization: "granted",
+      ad_storage: "granted",
+      analytics_storage: "granted",
+    });
+    const gtagScript = document.createElement("script");
+    gtagScript.async = true;
+    gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`;
+    const firstScript = document.getElementsByTagName("script")[0];
+    firstScript.parentNode.insertBefore(gtagScript, firstScript);
+  };
 
-  const toggleVisible = () => setBannerVisible((prevState) => !prevState);
+  const toggleBannerVisible = () => setBannerVisible((prevState) => !prevState);
 
   const updateCookies = () => {
-    toggleVisible();
+    toggleBannerVisible();
     const optInValues = [
       ...document.querySelectorAll("#cookieValues input:checked"),
     ].map((el: any) => el.value);
@@ -29,7 +45,33 @@ const CookieConsentBanner = () => {
       expires: 4320000000,
       path: "/",
     });
+    if (optInValues.includes("Analytics")) {
+      loadGtagJsScript();
+    }
   };
+
+  useEffect(() => {
+    const cookie = cookieCutter.get("ccbcookie");
+    if (!cookie) {
+      toggleBannerVisible();
+    }
+    (window as any).dataLayer = (window as any).dataLayer || [];
+    gtag("consent", "default", {
+      ad_user_data: "denied",
+      ad_personalization: "denied",
+      ad_storage: "denied",
+      analytics_storage: "denied",
+      wait_for_update: 500,
+    });
+    gtag("js", new Date());
+    gtag("config", GOOGLE_TAG_ID);
+    if (cookie && cookie.includes("Analytics")) {
+      (
+        document.querySelector("#analyticsCheckbox") as HTMLInputElement
+      ).checked = true;
+      loadGtagJsScript();
+    }
+  }, []);
 
   useEffect(() => {
     if (bannerRef.current) {
@@ -38,66 +80,64 @@ const CookieConsentBanner = () => {
   }, [bannerRef]);
 
   return (
-    bannerPresent && (
+    <div
+      className={classNames(styles.banner, { [styles.show]: bannerVisible })}
+      style={{ bottom: `${bannerHeight}px` }}
+    >
+      <div className="tag" onClick={toggleBannerVisible}>
+        <span className={styles.iconWrapper}>
+          <FontAwesomeIcon icon={faCookieBite} />
+        </span>
+      </div>
       <div
-        className={classNames(styles.banner, { [styles.show]: bannerVisible })}
-        style={{ bottom: `${bannerHeight}px` }}
+        className={styles.body}
+        role="dialog"
+        aria-label="cookieconsent"
+        aria-live="polite"
+        aria-describedby="cookieconsent"
+        ref={bannerRef}
       >
-        <div className="tag" onClick={toggleVisible}>
-          <span className={styles.iconWrapper}>
-            <FontAwesomeIcon icon={faCookieBite} />
-          </span>
-        </div>
-        <div
-          className={styles.body}
-          role="dialog"
-          aria-label="cookieconsent"
-          aria-live="polite"
-          aria-describedby="cookieconsent"
-          ref={bannerRef}
-        >
-          <span>
-            This site uses cookies. Some of them are essentials while other help
-            us improve your experience.&nbsp;
-            <Link
-              href="/about-impc/accessibility-cookies/"
-              className="primary link"
-              aria-label="learn more about cookies"
-              role="button"
-            >
-              Learn more
-            </Link>
-          </span>
-          <form id="cookieValues">
-            <label className={styles.checkbox}>
-              <input type="checkbox" value="Essential" checked disabled />
-              &nbsp;Essential
-            </label>
-            <label className={styles.checkbox}>
-              <input type="checkbox" value="Analytics" />
-              &nbsp;Analytics
-            </label>
-          </form>
-          <div className={styles.options}>
-            <button
-              className={classNames("btn", styles.button)}
-              aria-label="deny cookies"
-              onClick={toggleVisible}
-            >
-              Close
-            </button>
-            <button
-              id="saveCookies"
-              className={classNames("btn", styles.button, styles.allow)}
-              aria-label="allow cookies"
-              onClick={updateCookies}
-            >
-              Save preferences
-            </button>
-          </div>
+        <span>
+          This site uses cookies. Some of them are essentials while other help
+          us improve your experience.&nbsp;
+          <Link
+            href="/about-impc/accessibility-cookies/"
+            className="primary link"
+            aria-label="learn more about cookies"
+            role="button"
+          >
+            Learn more
+          </Link>
+        </span>
+        <form id="cookieValues">
+          <label className={styles.checkbox}>
+            <input type="checkbox" value="Essential" checked disabled />
+            &nbsp;Essential
+          </label>
+          <label className={styles.checkbox}>
+            <input id="analyticsCheckbox" type="checkbox" value="Analytics" />
+            &nbsp;Analytics
+          </label>
+        </form>
+        <div className={styles.options}>
+          <button
+            className={classNames("btn", styles.button)}
+            aria-label="deny cookies"
+            onClick={toggleBannerVisible}
+          >
+            Close
+          </button>
+          <button
+            id="saveCookies"
+            className={classNames("btn", styles.button, styles.allow)}
+            aria-label="allow cookies"
+            onClick={updateCookies}
+          >
+            Save preferences
+          </button>
         </div>
       </div>
-    )
+    </div>
   );
 };
 
