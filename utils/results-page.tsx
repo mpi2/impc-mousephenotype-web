2030e1b24c6cffcc56a9e19dd4442cfea67878c9
Francisco Peña <francisco@ebi.ac.uk>
2024-11-13 11:32:01 +0000

    fix: handle cases for indexOf not found case

--- utils/results-page.tsx
+++ utils/results-page.tsx
@@ -2,9 +2,12 @@ import DOMPurify from "dompurify";
 import ReactHtmlParser from "react-html-parser";
 export const surroundWithMarkEl = (text: string, query: string) => {
   const matchingSubsIndex = text.toLowerCase().indexOf(query.toLowerCase());
-  const originalTextMatchedQuery = DOMPurify.sanitize(
-    text.slice(matchingSubsIndex, matchingSubsIndex + query.length)
-  );
+  const originalTextMatchedQuery =
+    matchingSubsIndex !== -1
+      ? DOMPurify.sanitize(
+          text.slice(matchingSubsIndex, matchingSubsIndex + query.length)
+        )
+      : "";
   if (!!query) {
     return ReactHtmlParser(
       text.replaceAll(
