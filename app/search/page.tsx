21ecb0387a00d6c561aa96d3b223849997b75c00
Francisco Peña <francisco@ebi.ac.uk>
2025-01-22 13:00:03 +0000

    feat: add server component pages to embryo, vignettes and search pages
    To add metadata tags

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,91 +1,10 @@
-"use client";
-
-import { useRouter } from "next/router";
-import { useState } from "react";
-import {
-  AlleleResults,
-  GeneResults,
-  PhenotypeResults,
-  Search,
-} from "@/components";
 import { Metadata } from "next";
+import SearchPage from "./search-page";
 
 export const metadata: Metadata = {
   title: "IMPC Search | International Mouse Phenotyping Consortium",
 };
 
-const SearchResults = () => {
-  const router = useRouter();
-  const [query, setQuery] = useState((router.query.term as string) || "");
-  const { type } = router.query;
-
-  const renderResults = () => {
-    switch (type) {
-      case "phenotype":
-      case "pheno":
-        return <PhenotypeResults query={query} />;
-      case "allele":
-        return <AlleleResults query={query} />;
-      default:
-        return <GeneResults query={query} />;
-    }
-  };
-
-  const jsonLd = {
-    "@context": "http://schema.org",
-    "@type": "Dataset",
-    "@id": "http://www.mousephenotype.org",
-    name: "Mouse phenotype data of knockout mouse lines for protein-coding genes",
-    description:
-      "The International Mouse Phenotyping Consortium (IMPC) is systematically generating mouse knockouts for every protein-coding gene in the mouse genome (approx. 20,000 genes) and carries out high-throughput phenotyping of each line in order to determine gene function by determining the biological systems affected in the absence of the gene. This dataset contains all the genotype-to-phenotype associations, protocols, parameters and measurements currently generated using this approach.",
-    url: "http://www.mousephenotype.org",
-    keywords: "gene, phenotype, mouse, mammalian, human disease",
-    identifier: "DR21.0",
-    creator: {
-      "@type": "Organization",
-      name: "International Mouse Phenotyping Consortium",
-    },
-    provider: {
-      "@type": "Organization",
-      name: "International Mouse Phenotyping Consortium",
-    },
-    version: "21.0",
-    dateCreated: "2014",
-    dateModified: "2018",
-    citation:
-      "Dickinson et al. 2016. High-throughput discovery of novel developmental phenotypes. Nature 537, 508â€“514. PMID: 27626380. doi:10.1038/nature19356",
-    temporalCoverage: "2014..",
-    sameAs: "http://www.mousephenotype.org",
-    distribution: [
-      {
-        "@type": "DataDownload",
-        name: "MySQL database dump",
-        fileFormat: "application/octet-stream",
-        contentURL: "http://ftp.ebi.ac.uk/pub/databases/impc/release-21.0/",
-      },
-      {
-        "@type": "DataDownload",
-        name: "Binary Solr Schemas",
-        fileFormat: "application/octet-stream",
-        contentURL: "http://ftp.ebi.ac.uk/pub/databases/impc/release-21.0/",
-      },
-    ],
-  };
-
-  return (
-    <>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-      />
-      <Search updateURL onChange={setQuery} />
-      {renderResults()}
-    </>
-  );
-};
-
-export async function getServerSideProps(context) {
-  return { props: {} };
+export default async function Page() {
+  return <SearchPage />;
 }
-
-export default SearchResults;
---
+++ app/search/search-page.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { GeneResults, PhenotypeResults, Search } from "@/components";
+
+const SearchResults = () => {
+  const searchParams = useSearchParams();
+  const [query, setQuery] = useState(searchParams.get("term") || "");
+  const type = searchParams.get("type");
+
+  const renderResults = () => {
+    switch (type) {
+      case "phenotype":
+      case "pheno":
+        return <PhenotypeResults query={query} />;
+      default:
+        return <GeneResults query={query} />;
+    }
+  };
+
+  const jsonLd = {
+    "@context": "http://schema.org",
+    "@type": "Dataset",
+    "@id": "http://www.mousephenotype.org",
+    name: "Mouse phenotype data of knockout mouse lines for protein-coding genes",
+    description:
+      "The International Mouse Phenotyping Consortium (IMPC) is systematically generating mouse knockouts for every protein-coding gene in the mouse genome (approx. 20,000 genes) and carries out high-throughput phenotyping of each line in order to determine gene function by determining the biological systems affected in the absence of the gene. This dataset contains all the genotype-to-phenotype associations, protocols, parameters and measurements currently generated using this approach.",
+    url: "http://www.mousephenotype.org",
+    keywords: "gene, phenotype, mouse, mammalian, human disease",
+    identifier: "DR21.0",
+    creator: {
+      "@type": "Organization",
+      name: "International Mouse Phenotyping Consortium",
+    },
+    provider: {
+      "@type": "Organization",
+      name: "International Mouse Phenotyping Consortium",
+    },
+    version: "21.0",
+    dateCreated: "2014",
+    dateModified: "2018",
+    citation:
+      "Dickinson et al. 2016. High-throughput discovery of novel developmental phenotypes. Nature 537, 508â€“514. PMID: 27626380. doi:10.1038/nature19356",
+    temporalCoverage: "2014..",
+    sameAs: "http://www.mousephenotype.org",
+    distribution: [
+      {
+        "@type": "DataDownload",
+        name: "MySQL database dump",
+        fileFormat: "application/octet-stream",
+        contentURL: "http://ftp.ebi.ac.uk/pub/databases/impc/release-21.0/",
+      },
+      {
+        "@type": "DataDownload",
+        name: "Binary Solr Schemas",
+        fileFormat: "application/octet-stream",
+        contentURL: "http://ftp.ebi.ac.uk/pub/databases/impc/release-21.0/",
+      },
+    ],
+  };
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <Search updateURL onChange={setQuery} />
+      {renderResults()}
+    </>
+  );
+};
+
+export default SearchResults;
