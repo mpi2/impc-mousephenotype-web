e1f0181d012cd637d14d8a7929c2e4ec9b6fc251
Francisco Peña <francisco@ebi.ac.uk>
2025-01-27 15:06:33 +0000

    feat: update utils to use isomorphic-dompurify

--- utils/results-page.tsx
+++ utils/results-page.tsx
@@ -1,20 +1,20 @@
-import DOMPurify from "dompurify";
+import DOMPurify from "isomorphic-dompurify";
 import ReactHtmlParser from "react-html-parser";
-export const surroundWithMarkEl = (text: string, query: string) => {
-  const matchingSubsIndex = text.toLowerCase().indexOf(query.toLowerCase());
+export const surroundWithMarkEl = (text: string, query: string | undefined) => {
+  if (!query) {
+    return text;
+  }
+  const matchingSubsIndex = text.toLowerCase().indexOf(query?.toLowerCase());
   const originalTextMatchedQuery =
     matchingSubsIndex !== -1
       ? DOMPurify.sanitize(
-          text.slice(matchingSubsIndex, matchingSubsIndex + query.length)
+          text.slice(matchingSubsIndex, matchingSubsIndex + query.length),
         )
       : "";
-  if (!!query) {
-    return ReactHtmlParser(
-      text.replaceAll(
-        originalTextMatchedQuery,
-        `<mark style="padding: 0;">${originalTextMatchedQuery}</mark>`
-      )
-    );
-  }
-  return text;
+  return ReactHtmlParser(
+    text.replaceAll(
+      originalTextMatchedQuery,
+      `<mark style="padding: 0;">${originalTextMatchedQuery}</mark>`,
+    ),
+  );
 };
