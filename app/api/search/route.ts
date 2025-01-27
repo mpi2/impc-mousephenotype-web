caa3f828ddf182950e3513d7f771c23a5e708e15
Francisco Peña <francisco@ebi.ac.uk>
2025-01-27 16:50:02 +0000

    feat: added error handling to fetch

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -3,9 +3,15 @@ import { NextRequest } from "next/server";
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
   const prefix = searchParams.get("query");
-  const resp = await fetch(
-    `http://impc-search-service:8080/v1/search?prefix=${prefix}`,
-  );
+  let resp;
+  try {
+    resp = await fetch(
+      `http://impc-search-service:8080/v1/search?prefix=${prefix}`,
+    );
+  } catch (err) {
+    return Response.json({ status: 500, statusText: err.toString() });
+  }
+
   if (!resp.ok) {
     return Response.json({ status: resp.status, message: resp.statusText });
   }
