3da2f0804f9ac546e4e7d15cecf1a1c78c9cc099
Francisco Peña <francisco@ebi.ac.uk>
2025-02-03 16:39:07 +0000

    feat: update jest config to transform certain packages in test time

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,32 +1,38 @@
-import nextJest from 'next/jest.js';
+import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
-    dir: './',
+  dir: "./",
 });
 
 /** @type {import('jest').Config} */
 const config = {
-    testEnvironment: 'jest-environment-jsdom',
-    collectCoverage: true,
-    collectCoverageFrom: [
-        "./components/**/*.{ts,tsx}",
-        "./pages/**/*.{ts,tsx}",
-        "./shared/hooks/*.ts",
-        "!**/*.d.ts",
-        "!**/node_modules/**",
-    ],
-    coverageDirectory: './coverage',
-    testPathIgnorePatterns: [
-        "__tests__/utils.tsx",
-    ],
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    setupFiles: ['<rootDir>/jest.polyfills.js', "jest-canvas-mock"],
-    moduleNameMapper: {
-        '^@/components/(.*)$': '<rootDir>/components/$1',
-        '^@/pages/(.*)$': '<rootDir>/pages/$1',
-        '^@/utils/(.*)$': '<rootDir>/utils/$1',
-        '^d3$': '<rootDir>/node_modules/d3/dist/d3.min.js',
-        '^d3-color$': '<rootDir>/node_modules/d3-color/dist/d3-color.js',
-    }
+  testEnvironment: "jest-environment-jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "./components/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./shared/hooks/*.ts",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+  ],
+  coverageDirectory: "./coverage",
+  testPathIgnorePatterns: ["__tests__/utils.tsx"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFiles: ["<rootDir>/jest.polyfills.js", "jest-canvas-mock"],
+  moduleNameMapper: {
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/pages/(.*)$": "<rootDir>/pages/$1",
+    "^@/utils/(.*)$": "<rootDir>/utils/$1",
+    "^d3-(.+)$": "<rootDir>/node_modules/d3-$1/dist/d3-$1.js",
+    "^d3$": "<rootDir>/node_modules/d3/dist/d3.min.js",
+  },
+  transformIgnorePatterns: ["/node_modules/(?!(react-markdown|devlop))/"],
+};
+
+async function jestConfig() {
+  const nextJestConfig = await createJestConfig(config)();
+  nextJestConfig.transformIgnorePatterns[0] =
+    "/node_modules/(?!(react-markdown|devlop))/";
+  return nextJestConfig;
 }
-export default createJestConfig(config)
+export default jestConfig;
