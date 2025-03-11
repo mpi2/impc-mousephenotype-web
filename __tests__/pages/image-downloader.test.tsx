de8a42892d5817777b9cc0393f4a946d9437ce8f
Francisco Peña <francisco@ebi.ac.uk>
2025-03-11 12:39:13 +0000

    feat: add image downloader unit test page and snapshot

--- __tests__/pages/image-downloader.test.tsx
+++ __tests__/pages/image-downloader.test.tsx
@@ -1,5 +1,5 @@
 import ImageDownloaderPage from "@/app/genes/[pid]/download-images/[parameterStableId]/image-downloader-page";
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { API_URL, renderWithClient } from "../utils";
 import { server } from "../../mocks/server";
@@ -24,23 +24,35 @@ jest.mock("next/navigation", () => {
 });
 
 describe("Image Downloader Page", () => {
-  it("renders without crashing", () => {
+  it("renders without crashing", async () => {
     server.use(
       rest.get(
-        `${API_URL}/api/v1/images/find_by_mgi_and_stable_id?mgiGeneAccessionId=MGI:1929293&parameterStableId=IMPC_ABR_014_001`,
+        `${API_URL}/api/v1/images/find_by_mgi_and_stable_id`,
+        (req, res, ctx) => {
+          const paramStableId = req.url.searchParams.get("parameterStableId");
+          const mgiGeneAccessionId = req.url.searchParams.get("pid");
+          if (
+            paramStableId === "IMPC_ABR_014_001" &&
+            mgiGeneAccessionId === "MGI:1929293"
+          ) {
+            return res(ctx.json(cib2MutantImages));
+          }
+        },
+      ),
+    );
+    server.use(
+      rest.get(
+        `${API_URL}/api/v1/images/find_by_stable_id_and_sample_id`,
         (req, res, ctx) => {
           const paramStableId = req.url.searchParams.get("parameterStableId");
           const biologicalSampleGroup = req.url.searchParams.get(
             "biologicalSampleGroup",
           );
-          if (paramStableId === "IMPC_ABR_014_001") {
-            return res(
-              ctx.json(
-                biologicalSampleGroup === "control"
-                  ? cib2ControlImages
-                  : cib2MutantImages,
-              ),
-            );
+          if (
+            paramStableId === "IMPC_ABR_014_001" &&
+            biologicalSampleGroup === "control"
+          ) {
+            return res(ctx.json(cib2ControlImages));
           }
         },
       ),
@@ -51,6 +63,19 @@ describe("Image Downloader Page", () => {
         mutantImagesFromServer={[]}
       />,
     );
+    await waitFor(() =>
+      expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+        "Auditory Brain Stem Response / Click-evoked + 6 to 30kHz tone waveforms (pdf format)",
+      ),
+    );
+    await waitFor(async () => {
+      const rows = await screen.findAllByRole("table");
+      return expect(rows.length).toEqual(2);
+    });
+    await waitFor(async () => {
+      const rows = await screen.findAllByRole("row");
+      return expect(rows.length).toEqual(17);
+    });
     expect(container).toMatchSnapshot();
   });
 });
