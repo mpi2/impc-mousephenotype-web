45045a8c6cb1869eb54a361e833ee8675c37b9ea
Francisco Peña <francisco@ebi.ac.uk>
2025-03-11 15:47:59 +0000

    feat: initial Late Adult landing page unit test

---
+++ __tests__/components/LandingPages/late-adult.test.tsx
@@ -0,0 +1,25 @@
+import { within, screen, waitFor } from "@testing-library/react";
+import LateAdultLandingPage from "@/app/late-adult-data/late-adult-page";
+import { renderWithClient } from "../../utils";
+import userEvent from "@testing-library/user-event";
+
+window.ResizeObserver =
+  window.ResizeObserver ||
+  jest.fn().mockImplementation(() => ({
+    disconnect: jest.fn(),
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+  }));
+
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(),
+  useSearchParams: jest.fn().mockImplementation(() => new URLSearchParams()),
+  usePathname: jest.fn(),
+}));
+
+describe("Late Adult landing page", () => {
+  it("renders correctly", async () => {
+    const { container } = renderWithClient(<LateAdultLandingPage />);
+    expect(container).toMatchSnapshot();
+  });
+});
