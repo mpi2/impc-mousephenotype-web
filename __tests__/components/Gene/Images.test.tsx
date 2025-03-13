f92b1f03b1ec07d479d9bdb4d4d3492e1eee9cf0
Francisco Peña <francisco@ebi.ac.uk>
2025-03-13 13:54:47 +0000

    feat: update Gene order section unit test, added mock data

--- __tests__/components/Gene/Images.test.tsx
+++ __tests__/components/Gene/Images.test.tsx
@@ -4,9 +4,18 @@ import { renderWithClient, API_URL } from "../../utils";
 import { testServer } from "../../../mocks/server";
 import { rest } from "msw";
 import { GeneContext } from "@/contexts";
+import imageData from "../../../mocks/data/tests/crlf3-images-data.json";
 
 describe("Gene images component", () => {
   it("should display information", async () => {
+    testServer.use(
+      rest.get(
+        `${API_URL}/api/v1/genes/MGI:1860086/images`,
+        (req, res, ctx) => {
+          return res(ctx.json(imageData));
+        },
+      ),
+    );
     renderWithClient(
       <GeneContext.Provider
         value={{ geneSymbol: "Crlf3", mgiGeneAccessionId: "MGI:1860086" }}
@@ -15,7 +24,7 @@ describe("Gene images component", () => {
       </GeneContext.Provider>,
     );
     expect(screen.getByRole("heading")).toHaveTextContent("Associated images");
-    expect(await screen.findAllByTestId("image")).toHaveLength(5);
+    expect(await screen.findAllByTestId("image")).toHaveLength(8);
   });
 
   it("should show an error message if the request fails", async () => {
