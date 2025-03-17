0c3c4fada1d783208fe8b06150f3ac2f562dbb01
Francisco Peña <francisco@ebi.ac.uk>
2025-03-17 16:17:15 +0000

    feat: improved embryo and late adult snapshot tests, add mock rest server

--- __tests__/components/LandingPages/late-adult.test.tsx
+++ __tests__/components/LandingPages/late-adult.test.tsx
@@ -1,5 +1,9 @@
 import LateAdultLandingPage from "@/app/late-adult-data/late-adult-page";
 import { renderWithClient } from "../../utils";
+import { testServer } from "../../../mocks/server";
+import { rest } from "msw";
+import lateAdultData from "../../../mocks/data/tests/landing-pages/late-adult.json";
+import { screen, waitFor } from "@testing-library/react";
 
 window.ResizeObserver =
   window.ResizeObserver ||
@@ -17,7 +21,19 @@ jest.mock("next/navigation", () => ({
 
 describe("Late Adult landing page", () => {
   it("renders correctly", async () => {
+    testServer.use(
+      rest.get(
+        "https://impc-datasets.s3.eu-west-2.amazonaws.com/landing-page-data/dr22.1/late_adult_landing/procedure_level_data.json",
+        (_, res, ctx) => {
+          return res(ctx.json(lateAdultData));
+        },
+      ),
+    );
     const { container } = renderWithClient(<LateAdultLandingPage />);
+    await waitFor(async () => {
+      const tables = await screen.findAllByTestId("late-adult-heatmap");
+      return expect(tables.length).toEqual(1);
+    });
     expect(container).toMatchSnapshot();
   });
 });
