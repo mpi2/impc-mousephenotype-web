150cd406e5133ed5ad2a48dc84b770235472f196
Francisco Peña <francisco@ebi.ac.uk>
2025-04-04 18:45:19 +0100

    temporal: updated images route, transform images url

--- app/api/legacy-images/get-images-by-param-and-mgi/route.ts
+++ app/api/legacy-images/get-images-by-param-and-mgi/route.ts
@@ -1,4 +1,5 @@
 const endpointURL = "http://wp-p1m-d7.ebi.ac.uk:8986/solr/images/select";
+const imageURL = "http://hh-rke-wp-webadmin-20-worker-1.caas.ebi.ac.uk:30590/";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -7,8 +8,14 @@ export async function GET(request: Request) {
   if (!id || !procedureName) {
     return new Response("Missing parameter", { status: 400 });
   }
-  const url = `${endpointURL}?q=mgi_accession_id:"${id}" AND sangerProcedureName: "${procedureName}"`;
+  const url = `${endpointURL}?rows=1000&q=mgi_accession_id:"${id}" AND sangerProcedureName: "${procedureName}"`;
   const response = await fetch(url);
   const data = await response.json();
-  return Response.json(data);
+  const processedImages = data.response.docs.map((image) => ({
+    ...image,
+    fullResolutionFilePath: `${imageURL}${image.fullResolutionFilePath.substring(7)}`,
+    largeThumbnailFilePath: `${imageURL}${image.largeThumbnailFilePath.substring(7)}`,
+    smallThumbnailFilePath: `${imageURL}${image.smallThumbnailFilePath.substring(7)}`,
+  }));
+  return Response.json(processedImages);
 }
