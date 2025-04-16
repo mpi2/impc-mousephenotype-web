8a99e1dfa5852d94e4979d9a05a7b289e95f4c75
Francisco Peña <francisco@ebi.ac.uk>
2025-04-16 15:16:20 +0100

    feat: improve layout of external links, put each link on its own line

--- components/Gene/ExternalLinks/index.tsx
+++ components/Gene/ExternalLinks/index.tsx
@@ -1,11 +1,12 @@
 import { Card } from "@/components";
 import { useContext } from "react";
 import { GeneContext } from "@/contexts";
-import { Alert, Col, Container, Row } from "react-bootstrap";
+import { Alert, Container } from "react-bootstrap";
 import { useGeneExternalLinksQuery } from "@/hooks";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 import { sectionWithErrorBoundary } from "@/hoc/sectionWithErrorBoundary";
+import styles from "./styles.module.scss";
 
 const ExternalLinks = () => {
   const gene = useContext(GeneContext);
@@ -34,46 +35,39 @@ const ExternalLinks = () => {
     <Card id="external-links">
       <h2>External links</h2>
       <Container>
-        <Row>
+        <div>
           {providers?.map((provider, index) => (
-            <Col
-              key={index}
-              className="mb-3"
-              xs={12}
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-evenly",
-              }}
-            >
-              <div>
-                <b>{provider.providerName}</b>&nbsp;-&nbsp;
-                {provider.links.map((link) => (
-                  <a
-                    key={link.href}
-                    className="primary link"
-                    href={link.href}
-                    target="_blank"
-                    title={`visit ${provider.providerName} website for more details of ${link.label}`}
-                  >
-                    <i>{link.label}</i>
-                    <FontAwesomeIcon
-                      icon={faExternalLinkAlt}
-                      className="grey"
-                      size="xs"
-                      style={{ marginLeft: "0.3rem" }}
-                    />
-                  </a>
-                ))}
+            <div key={index} className="mb-4">
+              <div className={styles.provider}>
+                <div className={styles.providerName}>
+                  <b>{provider.providerName}</b>
+                </div>
+                <div className={styles.links}>
+                  {provider.links.map((link) => (
+                    <div className="single-link">
+                      <a
+                        key={link.href}
+                        className="primary link"
+                        href={link.href}
+                        target="_blank"
+                        title={`visit ${provider.providerName} website for more details of ${link.label}`}
+                      >
+                        <i>{link.label}</i>
+                        <FontAwesomeIcon
+                          icon={faExternalLinkAlt}
+                          className="grey"
+                          size="xs"
+                          style={{ marginLeft: "0.3rem" }}
+                        />
+                      </a>
+                      <ul className={styles.description}>{link.description}</ul>
+                    </div>
+                  ))}
+                </div>
               </div>
-              <ul
-                style={{ marginBottom: 0, display: "flex", columnGap: "1.3em" }}
-              >
-                {provider.links.map((link) => link.description)}
-              </ul>
-            </Col>
+            </div>
           ))}
-        </Row>
+        </div>
       </Container>
     </Card>
   );
