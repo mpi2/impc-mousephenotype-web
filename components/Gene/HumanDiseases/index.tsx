b20441c841cba5010732ead7634ad7f4a2c3d5dc
Diego Pava <93325413+dpavam@users.noreply.github.com>
2025-06-04 11:57:02 +0100

    Merge pull request #622 from mpi2/fix/phenogrid-predicted-associations-fix-dev
    fix:fullData to concatenate data even when associatedDiseases is empty

--- components/Gene/HumanDiseases/index.tsx
+++ components/Gene/HumanDiseases/index.tsx
@@ -351,7 +351,7 @@ const HumanDiseases = ({ initialData }: HumanDiseasesProps) => {
   const [tab, setTab] = useState("associated");
 
   const fullData = useMemo(
-    () => associatedDiseases?.concat(predictedDiseases ?? []) ?? [],
+    () => (associatedDiseases ?? []).concat(predictedDiseases ?? []) ?? [],
     [associatedDiseases, predictedDiseases],
   );
 
