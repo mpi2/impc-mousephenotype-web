7b58b5cbd37f4c22cde2533b5bb4b3c80b337154
Francisco Peña <francisco@ebi.ac.uk>
2025-09-25 15:53:41 +0100

    test: remove redirects

--- next.config.js
+++ next.config.js
@@ -17,21 +17,6 @@ module.exports = {
   basePath: "/data",
   //uncomment the following line when deploying with vercel
   // swcMinify: false,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/data",
-        permanent: true,
-        basePath: false,
-      },
-      {
-        source: "/",
-        destination: "/search",
-        permanent: true,
-      },
-    ];
-  },
   async headers() {
     return [
       {
