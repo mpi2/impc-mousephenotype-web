98acf36184d2c034468aba1d2d82094f77dccc0e
Francisco Peña <francisco@ebi.ac.uk>
2025-12-02 15:56:50 +0000

    feat: move CSP header from config to middleware, set upgradeInsecureRequests from env var

--- next.config.js
+++ next.config.js
@@ -1,37 +1,7 @@
-const isProd = process.env.NODE_ENV === "production";
-
-const cspHeader = `
-    default-src 'none';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.usercentrics.eu;
-    style-src 'self' 'unsafe-inline';
-    connect-src 'self' *.mousephenotype.org *.usercentrics.eu *.google.com *.ebi.ac.uk *.google-analytics.com *.amazonaws.com *.gentar.org ${!isProd ? "localhost:8010 localhost:5000" : ""};
-    img-src 'self' blob: data: *.usercentrics.eu *.ebi.ac.uk *.amazonaws.com *.google.co.uk;
-    frame-src *.usercentrics.eu;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`;
-
 module.exports = {
   basePath: "/data",
   //uncomment the following line when deploying with vercel
   // swcMinify: false,
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ""),
-          },
-        ],
-      },
-    ];
-  },
   output: "standalone",
   typescript: {
     // !! WARN !!
