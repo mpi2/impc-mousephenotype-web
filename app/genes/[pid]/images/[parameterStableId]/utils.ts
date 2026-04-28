1b78fbda790a657148022b2642c28987d32177ad
Francisco Peña <francisco@ebi.ac.uk>
2026-04-28 12:08:08 +0100

    feat: include most of existing studies in parameter map

--- app/genes/[pid]/images/[parameterStableId]/utils.ts
+++ app/genes/[pid]/images/[parameterStableId]/utils.ts
@@ -12,6 +12,23 @@ const bioStudiesIdParameterMap = {
   HIS: "S-BIAD3040", // Histopathology
   PAT: "S-BIAD3041", // Gross Pathology and Tissue Collection
   CSD: "S-BIAD3043", // Combined SHIRPA and Dismorphology
+  EEI: "S-BIAD3142", // Ear Epidermis Immunophenotyping
+  DSS: "S-BIAD3147", // DSS Histology
+  ANA: "S-BIAD3148", // Anti-nuclear Antibody Assay
+  GEP: "S-BIAD3151", // Gross Morphology Embryo E18.5
+  GEO: "S-BIAD3152", // Gross Morphology Embryo E14.5-E15.5
+  GEL: "S-BIAD3155", // Gross Morphology Embryo E9.5
+  EGR: "S-BIAD3212", // Electroretinography
+  GEM: "S-BIAD3213", // Gross Morphology Embryo E12.5
+  GPP: "S-BIAD3214", // Gross Morphology Placenta E18.5
+  GPO: "S-BIAD3215", // Gross Morphology Placenta E14.5-E15.5
+  GPL: "S-BIAD3217", // Gross Morphology Placenta E9.5
+  GPM: "S-BIAD3218", // Gross Morphology Placenta E12.5
+  IMM: "S-BIAD3234", // Spleen Immunophenotyping
+  MLN: "S-BIAD3235", // Mesenteric Lymph Node Immunophenotyping
+  BMI: "S-BIAD3236", // Bone Marrow Immunophenotyping
+  EML: "S-BIAD3243", // MicroCT E9.5
+  EMO: "S-BIAD3290", // MicroCT E14.5-E15.5
 };
 
 const bioStudiesKeys = Object.keys(bioStudiesIdParameterMap);
