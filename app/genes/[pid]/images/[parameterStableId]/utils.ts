6565f3db37c7ba661b15acabf653c36122795f31
Francisco Peña <francisco@ebi.ac.uk>
2026-04-28 10:54:56 +0100

    feat: sort data in parameter map, clarify each study contents

--- app/genes/[pid]/images/[parameterStableId]/utils.ts
+++ app/genes/[pid]/images/[parameterStableId]/utils.ts
@@ -1,17 +1,17 @@
 import { GeneImageCollection, Image } from "@/models/gene";
 
 const bioStudiesIdParameterMap = {
-  HIS: "S-BIAD3040",
-  CSD: "S-BIAD3043",
-  ECG: "S-BIAD3009",
-  SLW: "S-BIAD3012",
-  ALZ: "S-BIAD2986",
-  EYE: "S-BIAD2786",
-  PAT: "S-BIAD3041",
-  XRY: "S-BIAD2244",
-  ELZ: "S-BIAD3019",
-  ABR: "S-BIAD3008",
-  ECH: "S-BIAD2990",
+  XRY: "S-BIAD2244", // X-Ray
+  EYE: "S-BIAD2786", // Eye Morphology
+  ALZ: "S-BIAD2986", // Adult LacZ Expression
+  ECH: "S-BIAD2990", // Echocardiography
+  ABR: "S-BIAD3008", // Auditory Brain Stem Response
+  ECG: "S-BIAD3009", // Electrocardiogram
+  SLW: "S-BIAD3012", // Sleep Wake Monitoring
+  ELZ: "S-BIAD3019", // Embryo LacZ Expression
+  HIS: "S-BIAD3040", // Histopathology
+  PAT: "S-BIAD3041", // Gross Pathology and Tissue Collection
+  CSD: "S-BIAD3043", // Combined SHIRPA and Dismorphology
 };
 
 const bioStudiesKeys = Object.keys(bioStudiesIdParameterMap);
